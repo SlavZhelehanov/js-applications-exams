d17ca6ee47251cfc6f35efc68ab06a47992523f7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-16 18:18:38 +0300

    Add static "Login Page"

--- 01. JS Applications Regular Exam - 8 December 2024/resources/src/app.js
+++ 01. JS Applications Regular Exam - 8 December 2024/resources/src/app.js
@@ -3,6 +3,7 @@ import {render} from "../node_modules/lit-html/lit-html.js";
 import {layoutPage} from "./views/layouts/main.js";
 import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
+import {loginPage} from "./views/auth/login.js";
 
 const wrapper = document.getElementById('wrapper');
 
@@ -10,6 +11,7 @@ page(decorateContext)
 page("index.html", "/");
 page("/", homePage);
 page("/register", registerPage);
+page("/login", loginPage);
 
 page.start();
 
