9c17e6dc66be13108dde3018c50123991502e18e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-16 18:13:44 +0300

    Add static "Register Page"

--- 01. JS Applications Regular Exam - 8 December 2024/resources/src/app.js
+++ 01. JS Applications Regular Exam - 8 December 2024/resources/src/app.js
@@ -2,12 +2,16 @@ import page from "../node_modules/page/page.mjs";
 import {render} from "../node_modules/lit-html/lit-html.js";
 import {layoutPage} from "./views/layouts/main.js";
 import {homePage} from "./views/home/home.js";
+import {registerPage} from "./views/auth/register.js";
 
 const wrapper = document.getElementById('wrapper');
 
 page(decorateContext)
 page("index.html", "/");
 page("/", homePage);
+page("/register", registerPage);
+
+page.start();
 
 function decorateContext(ctx, next) {
     ctx.render = renderView;
