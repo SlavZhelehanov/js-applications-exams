c689b9c1f5e3079b70608242d6d2f8d6e13f0cae
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-28 13:29:50 +0200

    Search show by name

--- 02. JS Applications Retake Exam - 14 August 2024/resources/src/services/showsService.js
+++ 02. JS Applications Retake Exam - 14 August 2024/resources/src/services/showsService.js
@@ -1,27 +1,21 @@
-import {get, post, put} from "../utils/api.js";
 import {get, post, put, del} from "../utils/api.js";
 
 export async function getAllShows() {
     const path = '/data/shows?sortBy=_createdOn%20desc';
-    return get(path);
     return await get(path);
 }
 
 export async function getById(id) {
     const path = `/data/shows/${id}`;
-    return get(path);
     return await get(path);
 }
 
 export async function createShow(data) {
     const path = `/data/shows`;
-    return post(path, data);
     return await post(path, data);
 }
 
 export async function updateShow(id, data) {
-    const path = `/data/drones/${id}`;
-    return put(path, data);
     const path = `/data/shows/${id}`;
     return await put(path, data);
 }
@@ -30,4 +24,8 @@ export async function deleteShow(id) {
     const path = `/data/shows/${id}`;
     return await del(path);
 }
+
+export async function searchShow(query) {
+    const path = `/data/shows?where=title%20LIKE%20%22${query}%22`;
+    return await get(path);
 }
