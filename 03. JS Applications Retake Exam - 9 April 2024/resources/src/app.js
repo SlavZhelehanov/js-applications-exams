49b219b5fe0a28f6be3cc0b43015e077fc4aed58
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-29 14:50:00 +0200

    Add "Create Page"

--- 03. JS Applications Retake Exam - 9 April 2024/resources/src/app.js
+++ 03. JS Applications Retake Exam - 9 April 2024/resources/src/app.js
@@ -5,6 +5,7 @@ import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/solutions/dashboard.js";
 import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
+import {createPage} from "./views/solutions/create.js";
 
 const main = document.getElementsByTagName('main')[0];
 
@@ -20,5 +21,6 @@ page("/", homePage);
 page("/solutions", dashboardPage);
 page("/register", registerPage);
 page("/login", loginPage);
+page("/create", createPage);
 
 page.start();
