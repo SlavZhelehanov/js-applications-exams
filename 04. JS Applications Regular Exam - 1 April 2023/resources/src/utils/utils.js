0f2504fe50483797458800bce63396cb0d526a31
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-10-31 13:06:13 +0200

    Set navigation

--- 04. JS Applications Regular Exam - 1 April 2023/resources/src/utils/utils.js
+++ 04. JS Applications Regular Exam - 1 April 2023/resources/src/utils/utils.js
@@ -8,9 +8,14 @@ export function clearUserData() { sessionStorage.removeItem(item); }
 
 export function setNavigation() {
     const isLoggedIn = getUserData();
-    const [_, divUser, divGuest] = document.getElementsByTagName('nav')[0].getElementsByTagName('div');
+    // const [_, divUser, divGuest] = document.getElementsByTagName('nav')[0].getElementsByTagName('div');
+    const [_, authDiv] = document.getElementsByTagName('nav')[0].getElementsByTagName('div');
+    const [a1, a2] = authDiv.getElementsByTagName('a');
 
+    // isLoggedIn
+    //     ? [divUser.style.display = 'inline-block', divGuest.style.display = "none"]
+    //     : [divGuest.style.display = 'inline-block', divUser.style.display = "none"];
     isLoggedIn
-        ? [divUser.style.display = 'inline-block', divGuest.style.display = "none"]
-        : [divGuest.style.display = 'inline-block', divUser.style.display = "none"];
+        ? [authDiv.className = 'user', a1.href = '/addFruit', a2.href = '/logout', a1.textContent = 'Add Fruit', a2.textContent = 'Logout']
+        : [authDiv.className = 'guest', a1.href = '/login', a2.href = '/register', a1.textContent = 'Login', a2.textContent = 'Register'];
 }
