9366daf9bfa968f9a8ffea916d633c013dfb43e3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-04 14:18:55 +0200

    Add "Home Page"

--- 05. JS Applications Retake Exam - 11 April 2023/resources/src/app.js
+++ 05. JS Applications Retake Exam - 11 April 2023/resources/src/app.js
@@ -1,10 +1,13 @@
 import page from "./lib/page.mjs";
 
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
+import {homePage} from "./views/home/home.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
+page("/", homePage);
+
 
 page.start();
