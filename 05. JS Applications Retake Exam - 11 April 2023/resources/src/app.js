f9fac1543279ca7bab54d18f4327e478774b1b7e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-05 12:14:07 +0200

    Add hard coded "Dashboard Page"

--- 05. JS Applications Retake Exam - 11 April 2023/resources/src/app.js
+++ 05. JS Applications Retake Exam - 11 April 2023/resources/src/app.js
@@ -5,6 +5,7 @@ import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
 import {logoutAction} from "./views/auth/logout.js";
+import {dashboardPage} from "./views/events/dashboard.js";
 
 setNavigation();
 
@@ -14,5 +15,6 @@ page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/logout", guardRoute("user"), logoutAction);
+page("/events", dashboardPage);
 
 page.start();
