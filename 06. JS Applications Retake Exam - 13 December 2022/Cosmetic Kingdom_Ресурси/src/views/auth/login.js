0d32ad393b650b35003e5217eb4818b0757abe90
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-08 14:49:01 +0200

    Add "Login Page"

---
+++ 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/views/auth/login.js
@@ -0,0 +1,27 @@
+import { html } from '../../lib/lit-html.min.js';
+
+function template() {
+    return html`
+        <section id="login">
+            <div class="form">
+                <h2>Login</h2>
+                <form class="login-form">
+                    <input type="text" name="email" id="email" placeholder="email"/>
+                    <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="password"
+                    />
+                    <button type="submit">login</button>
+                    <p class="message">
+                        Not registered? <a href="/register">Create an account</a>
+                    </p>
+                </form>
+            </div>
+        </section>`
+}
+
+export function loginPage(ctx) {
+    ctx.render(template());
+}
