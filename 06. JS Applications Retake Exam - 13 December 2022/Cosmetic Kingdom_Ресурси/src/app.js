60167ced2a9d6f5232b853c379f70de47c1bf573
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-09 14:57:40 +0200

    Add hard coded "Details Page"

--- 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/app.js
+++ 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/app.js
@@ -7,6 +7,7 @@ import {loginPage} from "./views/auth/login.js";
 import {logoutAction} from "./views/auth/logout.js";
 import {dashboardPage} from "./views/products/dashboard.js";
 import {createPage} from "./views/products/create.js";
+import {detailsPage} from "./views/products/details.js";
 
 setNavigation();
 
@@ -18,5 +19,6 @@ page("/login", guardRoute("guest"), loginPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/products", dashboardPage);
 page("/addProduct", guardRoute("user"), createPage);
+page("/details/:id", detailsPage);
 
 page.start();
