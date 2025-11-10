d66b74015549a19727733a6897c2ed620b8afec3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-10 12:59:34 +0200

    Add dynamic data in "Dashboard Page"

--- 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/views/products/dashboard.js
+++ 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/views/products/dashboard.js
@@ -1,33 +1,30 @@
 import {html} from "../../lib/lit-html.min.js";
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(products) {
     return html`
         <h2>Products</h2>
-        <section id="dashboard">
-            <!-- Display a div with information about every post (if any)-->
-            <div class="product">
-                <img src="./images/product example 1.png" alt="example1"/>
-                <p class="title">Fond De Teint</p>
-                <p><strong>Price:</strong><span class="price">23.99</span>$</p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-            <div class="product">
-                <img src="./images/product example 2.png" alt="example1"/>
-                <p class="title">Eyeshadow Palette</p>
-                <p><strong>Price:</strong><span class="price">19.99</span>$</p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-            <div class="product">
-                <img src="./images/product example 3.png" alt="example1"/>
-                <p class="title">Lipstick</p>
-                <p><strong>Price:</strong><span class="price">16.99 </span>$</p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-        </section>
-        <!-- Display an h2 if there are no posts -->
-        <h2>No products yet.</h2>`;
+        ${0 < products.length
+                ? html`<section id="dashboard">
+            ${products.map(p => html`<div class="product">
+                    <img src=${p.imageUrl} alt="example1"/>
+                    <p class="title">${p.name}</p>
+                    <p><strong>Price:</strong><span class="price">${p.price}</span>$</p>
+                    <a class="details-btn" href="/details/${p._id}">Details</a>
+                </div>`)}
+        </section>`
+                : html`<h2>No products yet.</h2>`}
+    `;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let products = [];
+
+    try {
+        products = await get("/data/products?sortBy=_createdOn%20desc");
+    } catch (err) {
+        return alert(err.message);
+    }
+
+    ctx.render(template(products));
 }
