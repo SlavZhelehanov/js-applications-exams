7a1b16439956165bd8480e1f2c011eda7aceb637
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-10 13:29:50 +0200

    Edit / Update Product's info

--- 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/views/products/edit.js
+++ 06. JS Applications Retake Exam - 13 December 2022/Cosmetic Kingdom_Ресурси/src/views/products/edit.js
@@ -1,28 +1,32 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get, put} from '../../utils/api.js';
 
-function template() {
+function template(p, onEdit) {
     return html`
         <section id="edit">
             <div class="form">
                 <h2>Edit Product</h2>
-                <form class="edit-form">
+                <form class="edit-form" @submit=${onEdit}>
                     <input
                             type="text"
                             name="name"
                             id="name"
                             placeholder="Product Name"
+                            value=${p.name}
                     />
                     <input
                             type="text"
                             name="imageUrl"
                             id="product-image"
                             placeholder="Product Image"
+                            value=${p.imageUrl}
                     />
                     <input
                             type="text"
                             name="category"
                             id="product-category"
                             placeholder="Category"
+                            value=${p.category}
                     />
                     <textarea
                             id="product-description"
@@ -30,13 +34,14 @@ function template() {
                             placeholder="Description"
                             rows="5"
                             cols="50"
-                    ></textarea>
+                    >${p.description}</textarea>
 
                     <input
                             type="text"
                             name="price"
                             id="product-price"
                             placeholder="Price"
+                            value=${p.price}
                     />
                     <button type="submit">post</button>
                 </form>
@@ -45,5 +50,37 @@ function template() {
 }
 
 export async function editPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let product = {};
+
+    async function onEdit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const newProduct = {
+            name: formData.get('name').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            description: formData.get('description').trim(),
+            category: formData.get('category').trim(),
+            price: formData.get('price').trim()
+        }
+
+        if (Object.values(newProduct).some((x) => !x)) return alert("All fields are required!");
+
+        try {
+            await put(`/data/products/${id}`, newProduct);
+            e.target.reset();
+            ctx.page.redirect(`/details/${id}`);
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+    try {
+        product = await get(`/data/products/${id}`);
+    } catch (err) {
+        return alert(err.message);
+    }
+
+    ctx.render(template(product, onEdit));
 }
