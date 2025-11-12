2bc15d96d0146241c72adbc5ced8f002c5f67c14
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-12 12:24:52 +0200

    Add "Register Page"

---
+++ 07. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/src/views/auth/register.js
@@ -0,0 +1,36 @@
+import {html} from "../../lib/lit-html.min.js";
+
+function template() {
+    return html`
+        <section id="register">
+            <div class="form">
+                <h2>Register</h2>
+                <form class="login-form">
+                    <input
+                            type="text"
+                            name="email"
+                            id="register-email"
+                            placeholder="email"
+                    />
+                    <input
+                            type="password"
+                            name="password"
+                            id="register-password"
+                            placeholder="password"
+                    />
+                    <input
+                            type="password"
+                            name="re-password"
+                            id="repeat-password"
+                            placeholder="repeat password"
+                    />
+                    <button type="submit">login</button>
+                    <p class="message">Already registered? <a href="/login">Login</a></p>
+                </form>
+            </div>
+        </section>`;
+}
+
+export function registerPage(ctx) {
+    ctx.render(template());
+}
