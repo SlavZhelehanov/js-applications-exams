4bc0bce9a3f3e5205f7d3d7301ff2bf81f0c049e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-14 14:00:11 +0200

    Display correct data in "Details Page"

--- 07. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/src/views/shoes/details.js
+++ 07. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/src/views/shoes/details.js
@@ -1,21 +1,22 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(shoe) {
     return html`
         <section id="details">
             <div id="details-wrapper">
                 <p id="details-title">Shoe Details</p>
                 <div id="img-wrapper">
-                    <img src="./images/travis.jpg" alt="example1"/>
+                    <img src=${shoe.imageUrl} alt="example1"/>
                 </div>
                 <div id="info-wrapper">
-                    <p>Brand: <span id="details-brand">Air Jordan</span></p>
+                    <p>Brand: <span id="details-brand">${shoe.brand}</span></p>
                     <p>
-                        Model: <span id="details-model">1 Retro High TRAVIS SCOTT</span>
+                        Model: <span id="details-model">${shoe.model}</span>
                     </p>
-                    <p>Release date: <span id="details-release">2019</span></p>
-                    <p>Designer: <span id="details-designer">Travis Scott</span></p>
-                    <p>Value: <span id="details-value">2000</span></p>
+                    <p>Release date: <span id="details-release">${shoe.release}</span></p>
+                    <p>Designer: <span id="details-designer">${shoe.designer}</span></p>
+                    <p>Value: <span id="details-value">${shoe.value}</span></p>
                 </div>
 
                 <!--Edit and Delete are only for creator-->
@@ -28,5 +29,14 @@ function template() {
 }
 
 export async function detailsPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let shoe = {};
+
+    try {
+        shoe = await get(`/data/shoes/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(shoe));
 }
