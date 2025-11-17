b481996beb6a2834d9625422f591b6869df7a9cc
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-17 18:51:57 +0200

    Add "Home Page"

--- 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/app.js
+++ 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/app.js
@@ -1,10 +1,12 @@
 import page from "./lib/page.mjs";
 
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
+import {homePage} from "./views/home/home.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
+page("/", homePage);
 
 page.start();
