e2cd6cde4132e9a12694c3e86686f2350e0a2b04
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-18 14:01:51 +0200

    Login User

--- 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/auth/login.js
+++ 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/auth/login.js
@@ -1,11 +1,13 @@
 import {html} from '../../lib/lit-html.min.js';
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onLogin) {
     return html`
         <section id="login">
             <div class="form">
                 <h2>Login</h2>
-                <form class="login-form">
+                <form class="login-form" @submit=${onLogin}>
                     <input type="text" name="email" id="email" placeholder="email"/>
                     <input
                             type="password"
@@ -23,5 +25,27 @@ function template() {
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email.trim() === '' || password.trim() === '') return alert('All fields are required!');
+
+        try {
+            const user = await post("/users/login", {email, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
