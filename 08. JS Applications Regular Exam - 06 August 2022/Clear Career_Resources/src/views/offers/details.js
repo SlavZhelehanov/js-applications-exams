a19374e1de03cf6b0850ceffb17d39b5799b9cdb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-20 16:54:39 +0200

    Add dynamic data in "Details Page"

--- 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/offers/details.js
+++ 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/offers/details.js
@@ -1,54 +1,53 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get, del} from "../../utils/api.js";
 
-function template() {
+function template(offer, isOwner) {
     return html`
         <section id="details">
             <div id="details-wrapper">
-                <img id="details-img" src="./images/example2.png" alt="example1"/>
-                <p id="details-title">Senior Frontend Software Engineer</p>
-                <p id="details-category">
-                    Category: <span id="categories">IT, Developer, WEB</span>
-                </p>
-                <p id="details-salary">
-                    Salary: <span id="salary-number">7000</span>
-                </p>
+                <img id="details-img" src="${offer.imageUrl}" alt="example1"/>
+                <p id="details-title">${offer.title}</p>
+                <p id="details-category">Category: <span id="categories">${offer.category}</span></p>
+                <p id="details-salary">Salary: <span id="salary-number">${offer.salary}</span></p>
                 <div id="info-wrapper">
                     <div id="details-description">
                         <h4>Description</h4>
-                        <span
-                        >We are looking for programmers with a keen eye for design for
-                  the position of front end developer. Front end developers are
-                  responsible for ensuring the alignment of web design and user
-                  experience requirements, optimizing web pages for maximum
-                  efficiency, and maintaining brand consistency across all web
-                  pages, among other duties.</span
-                        >
+                        <span>${offer.description}</span>
                     </div>
                     <div id="details-requirements">
                         <h4>Requirements</h4>
-                        <span
-                        >Degree in computer science or related field. Understanding of
-                  key design principles. Proficiency in HTML, CSS, JavaScript.
-                  Experience with responsive and adaptive design. Good
-                  problem-solving skills. Excellent verbal communication skills.
-                  Good interpersonal skills.</span
-                        >
+                        <span>${offer.requirements}</span>
                     </div>
                 </div>
                 <p>Applications: <strong id="applications">1</strong></p>
 
                 <!--Edit and Delete are only for creator-->
                 <div id="action-buttons">
-                    <a href="" id="edit-btn">Edit</a>
-                    <a href="" id="delete-btn">Delete</a>
+                    ${isOwner
+                            ? html`
+                                <a href="/edit/${offer._id}" id="edit-btn">Edit</a>
+                                <a href="" id="delete-btn">Delete</a>`
+                            : null
+                    }
+                    
 
                     <!--Bonus - Only for logged-in users ( not authors )-->
-                    <a href="" id="apply-btn">Apply</a>
+                    <!--                    <a href="" id="apply-btn">Apply</a>-->
                 </div>
             </div>
         </section>`;
 }
 
 export async function detailsPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let offer = {}, isOwner = false;
+
+    try {
+        offer = await get(`/data/offers/${id}`);
+        isOwner = ctx.userData && offer._ownerId === ctx.userData._id;
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(offer, isOwner));
 }
