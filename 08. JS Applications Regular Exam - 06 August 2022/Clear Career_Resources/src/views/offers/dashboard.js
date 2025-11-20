23cc9f13b1f65e915ffac819aefa6d7412829049
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-20 16:35:50 +0200

    Add dynamic data in "Dashboard Page"

--- 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/offers/dashboard.js
+++ 08. JS Applications Regular Exam - 06 August 2022/Clear Career_Resources/src/views/offers/dashboard.js
@@ -1,44 +1,35 @@
 import {html} from "../../lib/lit-html.min.js";
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(offers) {
     return html`
         <section id="dashboard">
             <h2>Job Offers</h2>
 
-            <!-- Display a div with information about every post (if any)-->
-            <div class="offer">
-                <img src="./images/example1.png" alt="example1"/>
-                <p>
-                    <strong>Title: </strong><span class="title">Sales Manager</span>
-                </p>
-                <p><strong>Salary:</strong><span class="salary">1900</span></p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-            <div class="offer">
-                <img src="./images/example2.png" alt="example2"/>
-                <p>
-                    <strong>Title: </strong
-                    ><span class="title">Senior Frontend Software Engineer</span>
-                </p>
-                <p><strong>Salary:</strong><span class="salary">7000</span></p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-            <div class="offer">
-                <img src="./images/example3.png" alt="./images/example3.png"/>
-                <p>
-                    <strong>Title: </strong
-                    ><span class="title">Invoice Administrator</span>
-                </p>
-                <p><strong>Salary:</strong><span class="salary">1700</span></p>
-                <a class="details-btn" href="">Details</a>
-            </div>
-
-            <!-- Display an h2 if there are no posts -->
-            <h2>No offers yet.</h2>
+            ${0 < offers.length
+                    ? offers.map(o => html`
+                        <div class="offer">
+                            <img src="${o.imageUrl}" alt="example1"/>
+                            <p>
+                                <strong>Title: </strong><span class="title">${o.title}</span>
+                            </p>
+                            <p><strong>Salary:</strong><span class="salary">${o.salary}</span></p>
+                            <a class="details-btn" href="/details/${o._id}">Details</a>
+                        </div>`)
+                    : html`<h2>No offers yet.</h2>`
+            }
         </section>
     `;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let offers = [];
+
+    try {
+        offers = await get("/data/offers?sortBy=_createdOn%20desc");
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(offers));
 }
