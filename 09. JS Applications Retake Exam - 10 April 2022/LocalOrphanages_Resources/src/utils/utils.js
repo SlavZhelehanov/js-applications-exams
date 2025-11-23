ba1f1e25c9b76bd1a05faa1b4cd5d05f3d5b3400
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-23 16:39:32 +0200

    Set page navigation

---
+++ 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/utils/utils.js
@@ -0,0 +1,47 @@
+import {render} from "../lib/lit-html.min.js";
+import page from "../lib/page.mjs";
+
+const item = "userData";
+
+export function saveUserData(data) {
+    sessionStorage.setItem(item, JSON.stringify(data));
+}
+
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem(item));
+}
+
+export function clearUserData() {
+    sessionStorage.removeItem(item);
+}
+
+export function setNavigation() {
+    const isLoggedIn = Boolean(getUserData());
+    const authDiv = document.getElementsByTagName('nav')[0].getElementsByTagName('div')[0];
+    const [a1, a2] = authDiv.getElementsByTagName('a');
+
+    isLoggedIn
+        ? [authDiv.id = 'user', authDiv.innerHTML = '<a href="/my-posts">My Posts</a><a href="/create">Create Post</a><a href="/logout">Logout</a>']
+        : [authDiv.id = 'guest', authDiv.innerHTML = '<a href="/login">Login</a><a href="/register">Register</a>'];
+}
+
+export function decorateCTX(ctx, next) {
+    const main = document.getElementsByTagName('main')[0];
+
+    ctx.render = function (content) {
+        return render(content, main);
+    }
+    ctx.setNavigation = setNavigation;
+    ctx.userData = getUserData();
+    next();
+}
+
+export function guardRoute(status) {
+    return function (ctx, next) {
+        const user = getUserData();
+        const isUser = Boolean(user);
+
+        if ((status === 'user' && isUser) || (status === 'guest' && !isUser)) next();
+        else page.redirect('/');
+    };
+}
