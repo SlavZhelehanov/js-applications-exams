5b3c7cd6a50b002951381bd95bb719527d561745
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-24 19:30:09 +0200

    Login User

--- 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/views/auth/login.js
+++ 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/views/auth/login.js
@@ -1,9 +1,11 @@
 import {html} from '../../lib/lit-html.min.js';
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onLogin) {
     return html`
         <section id="login-page" class="auth">
-            <form id="login">
+            <form id="login" @submit=${onLogin}>
                 <h1 class="title">Login</h1>
 
                 <article class="input-group">
@@ -22,5 +24,27 @@ function template() {
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email.trim() === '' || password.trim() === '') return alert('All fields are required!');
+
+        try {
+            const user = await post("/users/login", {email, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
