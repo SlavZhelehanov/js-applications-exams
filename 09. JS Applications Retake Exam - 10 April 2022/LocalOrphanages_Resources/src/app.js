ee24164eeaa05d7268cbcbf6388cd6906ce636e1
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-24 19:19:56 +0200

    Add "Login Page"

--- 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/app.js
+++ 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/app.js
@@ -3,12 +3,14 @@ import page from "./lib/page.mjs";
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {dashboardPage} from "./views/app/dashboard.js";
 import {registerPage} from "./views/auth/register.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", dashboardPage);
-page("/register", guardRoute("guest"), registerPage)
+page("/register", guardRoute("guest"), registerPage);
+page("/login", guardRoute("guest"), loginPage);
 
 page.start();
