4fe994f434be96968a2d554b072b459ab740d42e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-25 13:03:07 +0200

    Add hard coded "Details Page"

--- 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/app.js
+++ 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/app.js
@@ -6,6 +6,7 @@ import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
 import {logoutAction} from "./views/auth/logout.js";
 import {createPage} from "./views/app/create.js";
+import {detailsPage} from "./views/app/details.js";
 
 setNavigation();
 
@@ -16,5 +17,6 @@ page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/create", guardRoute("user"), createPage);
+page("/details/:id", detailsPage);
 
 page.start();
