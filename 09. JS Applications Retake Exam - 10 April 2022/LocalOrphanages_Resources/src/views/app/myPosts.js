95a3bb14ae1d7b1f68ddbc835272dd5707410419
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-26 14:53:35 +0200

    Display correct data in "My Posts Page"
    And finalize the exam

--- 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/views/app/myPosts.js
+++ 09. JS Applications Retake Exam - 10 April 2022/LocalOrphanages_Resources/src/views/app/myPosts.js
@@ -1,34 +1,35 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <section id="my-posts-page">
             <h1 class="title">My Posts</h1>
 
-            <!-- Display a div with information about every post (if any)-->
-            <div class="my-posts">
-                <div class="post">
-                    <h2 class="post-title">Clothes</h2>
-                    <img class="post-image" src="./images/clothes.jpeg" alt="Material Image">
-                    <div class="btn-wrapper">
-                        <a href="#" class="details-btn btn">Details</a>
-                    </div>
-                </div>
-
-                <div class="post">
-                    <h2 class="post-title">Toys</h2>
-                    <img class="post-image" src="./images/toys.jpeg" alt="Material Image">
-                    <div class="btn-wrapper">
-                        <a href="#" class="details-btn btn">Details</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Display an h1 if there are no posts -->
-            <h1 class="title no-posts-title">You have no posts yet!</h1>
+            ${0 < data.length
+    ? html`<div class="my-posts">
+                        ${data.map(post => html`
+                            <div class="post">
+                                <h2 class="post-title">${post.title}</h2>
+                                <img class="post-image" src=${post.imageUrl} alt="Material Image">
+                                <div class="btn-wrapper">
+                                    <a href="/details/${post._id}" class="details-btn btn">Details</a>
+                                </div>
+                            </div>`)}
+                    </div>`
+                    : html`<h1 class="title no-posts-title">You have no posts yet!</h1>`
+    }
         </section>`;
 }
 
 export async function myPostsPage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get(`/data/posts?where=_ownerId%3D%22${ctx.userData._id}%22&sortBy=_createdOn%20desc`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(data));
 }
