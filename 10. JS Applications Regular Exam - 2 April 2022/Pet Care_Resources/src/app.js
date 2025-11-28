20df999e68d98f0a6e64d99f2be13b2372c8c878
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-11-28 12:30:18 +0200

    Logout User

--- 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/app.js
+++ 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/app.js
@@ -3,6 +3,7 @@ import page from "./lib/page.mjs";
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import { homePage } from "./views/home/home.js";
 import { registerPage } from "./views/auth/register.js";
+import { logoutAction } from "./views/auth/logout.js";
 
 setNavigation();
 
@@ -10,5 +11,6 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
+page("/logout", guardRoute("user"), logoutAction);
 
 page.start();
