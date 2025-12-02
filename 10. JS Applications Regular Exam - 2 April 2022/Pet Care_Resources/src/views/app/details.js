10cd50bd2c10746620d6aa02dbb2bd9ccdfa032d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-02 14:47:05 +0200

    Make donation

--- 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/details.js
+++ 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/details.js
@@ -1,7 +1,7 @@
 import {html} from '../../lib/lit-html.min.js';
-import {get} from "../../utils/api.js";
+import {get, post, del} from "../../utils/api.js";
 
-function template(item, isOwner, onDelete) {
+function template(item, isOwner, onDelete, isAuth, donated, canDonate, onDonate) {
     return html`
         <section id="detailsPage">
             <div class="details">
@@ -14,17 +14,18 @@ function template(item, isOwner, onDelete) {
                         <h3>Breed: ${item.breed}</h3>
                         <h4>Age: ${item.age}</h4>
                         <h4>Weight: ${item.weight}</h4>
-                        <h4 class="donation">Donation: 0$</h4>
+                        <h4 class="donation">Donation: ${donated * 100}$</h4>
                     </div>
-                    <!-- if there is no registered user, do not display div-->
                     <div class="actionBtn">
-                        ${isOwner
-                                ? html`<a href="#" class="edit">Edit</a>
-                                <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>`
-                                : null
+                        ${!isAuth
+                                ? null
+                                : isOwner
+                                        ? html`<a href="/edit/${item._id}" class="edit">Edit</a>
+                                        <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>`
+                                        : canDonate === 0
+                                                ? html`<a @click=${onDonate} href="javascript:void(0)" class="donate">Donate</a>`
+                                                : null
                         }
-                        <!--(Bonus Part) Only for no creator and user-->
-                        <a href="#" class="donate">Donate</a>
                     </div>
                 </div>
             </div>
@@ -33,7 +34,16 @@ function template(item, isOwner, onDelete) {
 
 export async function detailsPage(ctx) {
     const id = ctx.params.id;
-    let item = {}, isAuth = ctx.userData, isOwner = false;
+    let item = {}, isOwner = false, isAuth = ctx.userData, donated = 0, canDonate = 0;
+
+    async function onDonate() {
+        try {
+            await post(`/data/donation`, { petId: id });
+            ctx.page.redirect(`/details/${id}`);
+        } catch (error) {
+            alert(error.message);
+        }
+    }
 
     async function onDelete() {
         const choice = confirm('Are you sure?');
@@ -50,10 +60,13 @@ export async function detailsPage(ctx) {
 
     try {
         item = await get(`/data/pets/${id}`);
+        donated = await get(`/data/donation?where=petId%3D%22${id}%22&distinct=_ownerId&count`);
         isOwner = isAuth && item._ownerId === ctx.userData._id;
+
+        if (isAuth) canDonate = await get(`/data/donation?where=petId%3D%22${id}%22%20and%20_ownerId%3D%22${ctx.userData._id}%22&count`);
     } catch (err) {
         alert(err.message);
     }
 
-    ctx.render(template(item, isOwner, onDelete));
+    ctx.render(template(item, isOwner, onDelete, isAuth, donated, canDonate, onDonate));
 }
