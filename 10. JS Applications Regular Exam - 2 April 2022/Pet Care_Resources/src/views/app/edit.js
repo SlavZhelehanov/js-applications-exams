880b21e559e8830b002b1d7ce430a9974c230d92
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-02 14:50:23 +0200

    Show correct data in "Edti Page"

--- 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/edit.js
+++ 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/edit.js
@@ -1,31 +1,32 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get } from "../../utils/api.js";
 
-function template(item, onEdit) {
+function template(item) {
     return html`
         <section id="editPage">
             <form class="editForm">
-                <img src="./images/editpage-dog.jpg">
+                <img src="/images/editpage-dog.jpg">
                 <div>
                     <h2>Edit PetPal</h2>
                     <div class="name">
                         <label for="name">Name:</label>
-                        <input name="name" id="name" type="text" value="Max">
+                        <input name="name" id="name" type="text" value="${item.name}">
                     </div>
                     <div class="breed">
                         <label for="breed">Breed:</label>
-                        <input name="breed" id="breed" type="text" value="Shiba Inu">
+                        <input name="breed" id="breed" type="text" value="${item.breed}">
                     </div>
                     <div class="Age">
                         <label for="age">Age:</label>
-                        <input name="age" id="age" type="text" value="2 years">
+                        <input name="age" id="age" type="text" value="${item.age}">
                     </div>
                     <div class="weight">
                         <label for="weight">Weight:</label>
-                        <input name="weight" id="weight" type="text" value="5kg">
+                        <input name="weight" id="weight" type="text" value="${item.weight}">
                     </div>
                     <div class="image">
                         <label for="image">Image:</label>
-                        <input name="image" id="image" type="text" value="./image/dog.jpeg">
+                        <input name="image" id="image" type="text" value="${item.image}">
                     </div>
                     <button class="btn" type="submit">Edit Pet</button>
                 </div>
@@ -34,5 +35,14 @@ function template(item, onEdit) {
 }
 
 export async function editPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    try {
+        item = await get(`/data/pets/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(item));
 }
