7e07279944129de6d06478ec51ec340e0c1db38a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-02 14:28:59 +0200

    Add dynamic data in "Dashboard Page"

--- 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/dashboard.js
+++ 10. JS Applications Regular Exam - 2 April 2022/Pet Care_Resources/src/views/app/dashboard.js
@@ -1,59 +1,38 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(items) {
     return html`
         <section id="dashboard">
             <h2 class="dashboard-title">Services for every animal</h2>
             <div class="animals-dashboard">
-                <div class="animals-board">
-                    <article class="service-img">
-                        <img class="animal-image-cover" src="./images/cat2.jpg">
-                    </article>
-                    <h2 class="name">Athena</h2>
-                    <h3 class="breed">American Curl</h3>
-                    <div class="action">
-                        <a class="btn" href="#">Details</a>
-                    </div>
-                </div>
-
-                <div class="animals-board">
-                    <article class="service-img">
-                        <img class="animal-image-cover" src="./images/dog2.jpg">
-                    </article>
-                    <h2 class="name">Apollo</h2>
-                    <h3 class="breed">Pug</h3>
-                    <div class="action">
-                        <a class="btn" href="#">Details</a>
-                    </div>
-                </div>
-
-                <div class="animals-board">
-                    <img class="animal-image-cover" src="./images/guinea-pig.jpg">
-                    <h2 class="name">Chibi</h2>
-                    <h3 class="breed">Teddy guinea pig</h3>
-                    <div class="action">
-                        <a class="btn" href="#">Details</a>
-                    </div>
-                </div>
-
-                <div class="animals-board">
-                    <article class="service-img">
-                        <img class="animal-image-cover" src="./images/Shiba-Inu.png">
-                    </article>
-                    <h2 class="name">Max</h2>
-                    <h3 class="breed">Shiba Inu</h3>
-                    <div class="action">
-                        <a class="btn" href="#">Details</a>
-                    </div>
-                </div>
-                <!--If there is no pets in dashboard-->
-                <div>
-                    <p class="no-pets">No pets in dashboard</p>
-                </div>
+                ${0 < items.length
+                        ? items.map(i => html`
+                        <div class="animals-board">
+                            <article class="service-img">
+                                <img class="animal-image-cover" src=${i.image}>
+                            </article>
+                            <h2 class="name">${i.name}</h2>
+                            <h3 class="breed">${i.breed}</h3>
+                            <div class="action">
+                                <a class="btn" href="/details/${i._id}">Details</a>
+                            </div>
+                        </div>`)
+                        : html`<div><p class="no-pets">No pets in dashboard</p></div>`
+                }
             </div>
-        </section>`;
+        </section>
+    `;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let items = [];
+
+    try {
+        items = await get("/data/pets?sortBy=_createdOn%20desc&distinct=name");
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(items));
 }
