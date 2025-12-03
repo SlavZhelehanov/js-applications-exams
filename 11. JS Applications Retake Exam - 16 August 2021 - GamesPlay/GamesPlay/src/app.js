1a83c74c85eed205688108464a9e4963eec26333
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-03 16:23:01 +0200

    Logout User

--- 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/app.js
+++ 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/app.js
@@ -4,6 +4,7 @@ import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
+import {logoutAction} from "./views/auth/logout.js";
 
 setNavigation();
 
@@ -12,4 +13,5 @@ page("/index.html", "/");
 page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
+page("/logout", guardRoute("user"), logoutAction);
 page.start();
