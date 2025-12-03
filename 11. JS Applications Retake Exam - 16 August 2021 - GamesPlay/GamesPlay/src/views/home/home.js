a7a6e80835086cc5238fa24859327f8b77012928
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-03 16:12:33 +0200

    Show dynamic data in "Home Page"

--- 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/home/home.js
+++ 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/home/home.js
@@ -1,9 +1,9 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <section id="welcome-world">
-
             <div class="welcome-message">
                 <h2>ALL new games are</h2>
                 <h3>Only in GamesPlay</h3>
@@ -13,50 +13,32 @@ function template() {
             <div id="home-page">
                 <h1>Latest Games</h1>
 
-                <!-- Display div: with information about every game (if any) -->
-                <div class="game">
+                ${0 < data.length
+            ? data.map(g => html`<div class="game">
                     <div class="image-wrap">
-                        <img src="./images/CoverFire.png">
+                        <img src=${g.imageUrl}>
                     </div>
-                    <h3>Cover Fire</h3>
+                    <h3>${g.title}</h3>
                     <div class="rating">
                         <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
                     </div>
                     <div class="data-buttons">
-                        <a href="#" class="btn details-btn">Details</a>
+                        <a href="/details/${g._id}" class="btn details-btn">Details</a>
                     </div>
-                </div>
-                <div class="game">
-                    <div class="image-wrap">
-                        <img src="./images/ZombieLang.png">
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div class="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div>
-                    <div class="data-buttons">
-                        <a href="#" class="btn details-btn">Details</a>
-                    </div>
-                </div>
-                <div class="game">
-                    <div class="image-wrap">
-                        <img src="./images/MineCraft.png">
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div class="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                    </div>
-                    <div class="data-buttons">
-                        <a href="#" class="btn details-btn">Details</a>
-                    </div>
-                </div>
-
-                <!-- Display paragraph: If there is no games  -->
-                <p class="no-articles">No games yet</p>
+                </div>`)
+            : html`<p class="no-articles">No games yet</p>`
+        }
             </div>
         </section>`;
 }
 
 export async function homePage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get(`/data/games?sortBy=_createdOn%20desc&distinct=category`);
+    } catch (error) {
+        alert(error.message);
+    }
+    ctx.render(template(data));
 }
