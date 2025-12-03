cad5ff52fd6201e22203e96fb28b910ea458b48b
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-03 16:17:56 +0200

    Register User

--- 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/auth/register.js
+++ 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/auth/register.js
@@ -1,9 +1,11 @@
 import {html} from "../../lib/lit-html.min.js";
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onRegister) {
     return html`
         <section id="register-page" class="content auth">
-            <form id="register">
+            <form id="register" @submit=${onRegister}>
                 <div class="container">
                     <div class="brand-logo"></div>
                     <h1>Register</h1>
@@ -20,7 +22,7 @@ function template() {
                     <input class="btn submit" type="submit" value="Register">
 
                     <p class="field">
-                        <span>If you already have profile click <a href="#">here</a></span>
+                        <span>If you already have profile click <a href="/login">here</a></span>
                     </p>
                 </div>
             </form>
@@ -28,5 +30,30 @@ function template() {
 }
 
 export function registerPage(ctx) {
-    ctx.render(template());
+    async function onRegister(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const repass = formData.get('confirm-password');
+
+        if (email === '' || password === '') return alert('All fields are required');
+        if (password !== repass) return alert("Passwords don't match");
+
+        try {
+            const user = await post("/users/register", {email, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            if(err.message) alert(err.message);
+            else alert(err);
+        }
+    }
+
+    ctx.render(template(onRegister));
 }
