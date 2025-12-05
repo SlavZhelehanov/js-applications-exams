a52b99a93e6021b882718a4156496e6ae02a31ef
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-05 03:18:30 +0200

    Show dynamic data in "Dashboard / Catalog Page"

--- 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/app/dashboard.js
+++ 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/app/dashboard.js
@@ -1,42 +1,34 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <section id="catalog-page">
             <h1>All Games</h1>
-            <!-- Display div: with information about every game (if any) -->
-            <div class="allGames">
+            ${0 < data.length
+            ? data.map(g => html`<div class="allGames">
                 <div class="allGames-info">
-                    <img src="./images/avatar-1.jpg">
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" class="details-button">Details</a>
+                    <img src=${g.imageUrl}>
+                    <h6>${g.category}</h6>
+                    <h2>${g.title}</h2>
+                    <a href="/details/${g._id}" class="details-button">Details</a>
                 </div>
-
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="./images/avatar-1.jpg">
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="./images/avatar-1.jpg">
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-            </div>
-
-            <!-- Display paragraph: If there is no games  -->
-            <h3 class="no-articles">No articles yet</h3>
-        </section>`;
+            </div>`
+            )
+            : html`<h3 class="no-articles">No articles yet</h3>`
+        }
+        </section>
+    `;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get("/data/games?sortBy=_createdOn%20desc");
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(data));
 }
