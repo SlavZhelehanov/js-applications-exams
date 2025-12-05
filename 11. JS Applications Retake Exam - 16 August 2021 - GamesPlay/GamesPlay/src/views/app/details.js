687d6930ba95a775fa2fa45f589ad803c05f4b2c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-05 03:21:42 +0200

    Show correct data in "Details Page"

--- 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/app/details.js
+++ 11. JS Applications Retake Exam - 16 August 2021 - GamesPlay/GamesPlay/src/views/app/details.js
@@ -1,51 +1,30 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get, del, post } from "../../utils/api.js";
 
-function template() {
+function template(item, isOwner, onDelete, isAuth, comments, makeComment) {
     return html`
         <section id="game-details">
             <h1>Game Details</h1>
             <div class="info-section">
 
                 <div class="game-header">
-                    <img class="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span class="levels">MaxLevel: 4</span>
-                    <p class="type">Action, Crime, Fantasy</p>
+                    <img class="game-img" src=${item.imageUrl} />
+                    <h1>${item.title}</h1>
+                    <span class="levels">MaxLevel: ${item.maxLevel}</span>
+                    <p class="type">${item.category}</p>
                 </div>
 
-                <p class="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find a weapon everyone is prepared to kill for.
-                </p>
 
                 <!-- Bonus ( for Guests and Users ) -->
                 <div class="details-comments">
                     <h2>Comments:</h2>
-                    <ul>
-                        <!-- list all comments for current game (If any) -->
-                        <li class="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li class="comment">
-                            <p>Content: The best game.</p>
-                        </li>
-                    </ul>
-                    <!-- Display paragraph: If there are no games in the database -->
-                    <p class="no-comment">No comments.</p>
                 </div>
 
                 <!-- Edit/Delete buttons ( Only for creator of this game )  -->
                 <div class="buttons">
-                    <a href="#" class="button">Edit</a>
-                    <a href="#" class="button">Delete</a>
                 </div>
             </div>
 
-            <!-- Bonus -->
-            <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
-            <article class="create-comment">
                 <label>Add new comment:</label>
                 <form class="form">
                     <textarea name="comment" placeholder="Comment......"></textarea>
@@ -57,5 +36,15 @@ function template() {
 }
 
 export async function detailsPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id, isAuth = !!ctx.userData;
+    let item = {}, isOwner = false, comments = [];
+
+
+    try {
+        item = await get(`/data/games/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(item, isOwner, onDelete, isAuth, comments, makeComment));
 }
