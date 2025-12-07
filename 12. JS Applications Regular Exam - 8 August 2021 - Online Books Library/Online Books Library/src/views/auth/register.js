20c06c7c381647a7c67ed71bebf1a978f4f2bfa6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-07 17:00:21 +0200

    Register User

--- 12. JS Applications Regular Exam - 8 August 2021 - Online Books Library/Online Books Library/src/views/auth/register.js
+++ 12. JS Applications Regular Exam - 8 August 2021 - Online Books Library/Online Books Library/src/views/auth/register.js
@@ -1,9 +1,11 @@
 import {html} from "../../lib/lit-html.min.js";
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onRegister) {
     return html`
         <section id="register-page" class="register">
-            <form id="register-form" action="" method="">
+            <form id="register-form" @submit=${onRegister}>
                 <fieldset>
                     <legend>Register Form</legend>
                     <p class="field">
@@ -31,5 +33,30 @@ function template() {
 }
 
 export function registerPage(ctx) {
-    ctx.render(template());
+    async function onRegister(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const repass = formData.get('confirm-pass');
+
+        if (email === '' || password === '') return alert('All fields are required');
+        if (password !== repass) return alert("Passwords don't match");
+
+        try {
+            const user = await post("/users/register", {email, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            if(err.message) alert(err.message);
+            else alert(err);
+        }
+    }
+
+    ctx.render(template(onRegister));
 }
