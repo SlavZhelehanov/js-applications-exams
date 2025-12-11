cac58813876992d18e9fed910a07f502a1d741ff
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-11 12:38:16 +0200

    Add "Login Page"

--- 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/app.js
+++ 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/app.js
@@ -4,6 +4,7 @@ import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard.js";
 import {registerPage} from "./views/auth/register.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 
@@ -11,6 +12,7 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/app", dashboardPage);
-page("/register", guardRoute("guest"), registerPage)
+page("/register", guardRoute("guest"), registerPage);
+page("/login", guardRoute("guest"), loginPage);
 
 page.start();
