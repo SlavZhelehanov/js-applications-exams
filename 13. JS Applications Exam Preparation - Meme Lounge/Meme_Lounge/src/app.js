62367f5e106c4398aa9bd23dd633fa4da1f2bb98
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-11 12:33:46 +0200

    Add "Register Page"

--- 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/app.js
+++ 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/app.js
@@ -3,12 +3,14 @@ import page from "./lib/page.js";
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard.js";
+import {registerPage} from "./views/auth/register.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
-page("/app", dashboardPage)
+page("/app", dashboardPage);
+page("/register", guardRoute("guest"), registerPage)
 
 page.start();
