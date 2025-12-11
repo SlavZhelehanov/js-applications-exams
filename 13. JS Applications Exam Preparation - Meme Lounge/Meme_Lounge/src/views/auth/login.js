f8fd327cbe8aa365bea66a2d4304c5f8e688c5ee
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-11 12:42:12 +0200

    Login User

--- 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/views/auth/login.js
+++ 13. JS Applications Exam Preparation - Meme Lounge/Meme_Lounge/src/views/auth/login.js
@@ -1,9 +1,11 @@
 import { html } from '../../lib/lit-html.min.js';
+import { post } from "../../utils/api.js";
+import { saveUserData, showError } from "../../utils/utils.js";
 
 function template(onLogin) {
     return html`
         <section id="login">
-            <form id="login-form">
+            <form id="login-form" @submit=${onLogin}>
                 <div class="container">
                     <h1>Login</h1>
                     <label for="email">Email</label>
@@ -12,7 +14,7 @@ function template(onLogin) {
                     <input id="password" type="password" placeholder="Enter Password" name="password">
                     <input type="submit" class="registerbtn button" value="Login">
                     <div class="container signin">
-                        <p>Dont have an account?<a href="#">Sign up</a>.</p>
+                        <p>Dont have an account?<a href="/register">Sign up</a>.</p>
                     </div>
                 </div>
             </form>
@@ -20,5 +22,27 @@ function template(onLogin) {
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email.trim() === '' || password.trim() === '') return showError('All fields are required!');
+
+        try {
+            const user = await post("/users/login", { email, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/app');
+        } catch (err) {
+            showError(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
