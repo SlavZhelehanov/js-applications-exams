c1bf40e3164ad5d9acf9eeb4458fd2826cf5c5c2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-17 17:14:08 +0200

    Login User

--- 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/auth/login.js
+++ 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/auth/login.js
@@ -1,9 +1,11 @@
 import { html } from '../../lib/lit-html.min.js';
+import { post } from "../../utils/api.js";
+import { saveUserData } from "../../utils/utils.js";
 
 function template(onLogin) {
     return html`
         <section id="loginaPage">
-            <form class="loginForm">
+            <form class="loginForm" @submit=${onLogin}>
                 <h2>Login</h2>
                 <div>
                     <label for="email">Email:</label>
@@ -17,12 +19,34 @@ function template(onLogin) {
                 <button class="btn" type="submit">Login</button>
 
                 <p class="field">
-                    <span>If you don't have profile click <a href="#">here</a></span>
+                    <span>If you don't have profile click <a href="/register">here</a></span>
                 </p>
             </form>
-        </section>`
+        </section>`;
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email.trim() === '' || password.trim() === '') return alert('All fields are required!');
+
+        try {
+            const user = await post("/users/login", { email, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
