a1834033e82dc2baec5f6dc1d71b24b79189e19f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-17 17:13:42 +0200

    Register User

--- 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/auth/register.js
+++ 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/auth/register.js
@@ -1,9 +1,11 @@
 import { html } from "../../lib/lit-html.min.js";
+import { post } from "../../utils/api.js";
+import { saveUserData } from "../../utils/utils.js";
 
 function template(onRegister) {
     return html`
         <section id="registerPage">
-            <form class="registerForm">
+            <form class="registerForm" @submit=${onRegister}>
                 <h2>Register</h2>
                 <div class="on-dark">
                     <label for="email">Email:</label>
@@ -23,12 +25,37 @@ function template(onRegister) {
                 <button class="btn" type="submit">Register</button>
 
                 <p class="field">
-                    <span>If you have profile click <a href="#">here</a></span>
+                    <span>If you have profile click <a href="/login">here</a></span>
                 </p>
             </form>
         </section>`;
 }
 
 export function registerPage(ctx) {
-    ctx.render(template());
+    async function onRegister(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const repass = formData.get('repeatPassword');
+
+        if (email === '' || password === '') return alert('All fields are required');
+        if (password !== repass) return alert("Passwords don't match");
+
+        try {
+            const user = await post("/users/register", { email, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            if (err.message) alert(err.message);
+            else alert(err);
+        }
+    }
+
+    ctx.render(template(onRegister));
 }
