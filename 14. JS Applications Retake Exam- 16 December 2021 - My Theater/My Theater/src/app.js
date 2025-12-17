c948cf4790edd4c41ec937bd58dbcbfbb28c5935
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-17 17:13:18 +0200

    Add "Login Page"

--- 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/app.js
+++ 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/app.js
@@ -3,12 +3,14 @@ import page from "./lib/page.mjs";
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
-page("/register", guardRoute("guest"), registerPage)
+page("/register", guardRoute("guest"), registerPage);
+page("/login", guardRoute("guest"), loginPage);
 
 page.start();
