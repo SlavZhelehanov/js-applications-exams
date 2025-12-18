e5c6c06d625f7a3678a6800d352c549b36130388
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-18 17:49:05 +0200

    Add "Edit Page"

--- 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/app.js
+++ 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/app.js
@@ -6,6 +6,7 @@ import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
 import {logoutAction} from "./views/auth/logout.js";
 import {createPage} from "./views/app/create.js";
+import {editPage} from "./views/app/edit.js";
 
 setNavigation();
 
@@ -16,5 +17,6 @@ page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/create", guardRoute("user"), createPage);
+page("/edit/:id", guardRoute("user"), editPage);
 
 page.start();
