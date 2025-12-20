3d458378c4cd3bd026cba80ed62e8d2916438c79
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-20 21:31:00 +0200

    Edit Theater

--- 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/app/edit.js
+++ 14. JS Applications Retake Exam- 16 December 2021 - My Theater/My Theater/src/views/app/edit.js
@@ -1,32 +1,30 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get, put } from "../../utils/api.js";
 
-function template() {
+function template(item, onEdit) {
     return html`
         <section id="editPage">
-            <form class="theater-form">
+            <form class="theater-form" @submit=${onEdit}>
                 <h1>Edit Theater</h1>
                 <div>
                     <label for="title">Title:</label>
-                    <input id="title" name="title" type="text" placeholder="Theater name" value="To Kill A Mockingbird">
+                    <input id="title" name="title" type="text" placeholder="Theater name" value=${item.title}>
                 </div>
                 <div>
                     <label for="date">Date:</label>
-                    <input id="date" name="date" type="text" placeholder="Month Day, Year" value="December 13, 2018">
+                    <input id="date" name="date" type="text" placeholder="Month Day, Year" value=${item.date}>
                 </div>
                 <div>
                     <label for="author">Author:</label>
-                    <input id="author" name="author" type="text" placeholder="Author"
-                        value="Aaron Sorkin, Fred Fordham">
+                    <input id="author" name="author" type="text" placeholder="Author" value=${item.author}>
                 </div>
                 <div>
                     <label for="description">Theater Description:</label>
-                    <textarea id="description" name="description"
-                        placeholder="Description">To Kill a Mockingbird is a 2018 play based on the 1960 novel of the same name by Harper Lee, adapted for the stage by Aaron Sorkin. It opened on Broadway at the Shubert Theatre on December 13, 2018. The play is set to transfer to London's West End at the Gielgud Theatre in March 2022.</textarea>
+                    <textarea id="description" name="description" placeholder="Description">${item.description}</textarea>
                 </div>
                 <div>
                     <label for="imageUrl">Image url:</label>
-                    <input id="imageUrl" name="imageUrl" type="text" placeholder="Image Url"
-                        value="./images/Moulin-Rouge!-The-Musical.jpg">
+                    <input id="imageUrl" name="imageUrl" type="text" placeholder="Image Url" value=${item.imageUrl}>
                 </div>
                 <button class="btn" type="submit">Submit</button>
             </form>
@@ -34,5 +32,33 @@ function template() {
 }
 
 export async function editPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    async function onEdit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const newItem = {
+            title: formData.get('title').trim(),
+            date: formData.get('date').trim(),
+            author: formData.get('author').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            description: formData.get('description').trim()
+        }
+
+        if (Object.values(newItem).some((x) => !x)) return alert("All fields are required!");
+
+        await put(`/data/theaters/${id}`, newItem);
+        e.target.reset();
+        ctx.page.redirect(`/details/${id}`);
+    }
+
+    try {
+        item = await get(`/data/theaters/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(item, onEdit));
 }
