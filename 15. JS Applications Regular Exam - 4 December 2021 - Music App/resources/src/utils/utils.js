62750482d8f3f40d640194f54056a14357824e2c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-22 12:30:30 +0200

    Set App navigation

---
+++ 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/utils/utils.js
@@ -0,0 +1,46 @@
+import {render} from "../lib/lit-html.min.js";
+import page from "../lib/page.mjs";
+
+const item = "userData";
+
+export function saveUserData(data) {
+    sessionStorage.setItem(item, JSON.stringify(data));
+}
+
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem(item));
+}
+
+export function clearUserData() {
+    sessionStorage.removeItem(item);
+}
+
+export function setNavigation() {
+    const isLoggedIn = Boolean(getUserData());
+    const authUl = document.getElementsByTagName('nav')[0].getElementsByTagName('ul')[0];    
+
+    isLoggedIn
+        ? authUl.innerHTML = '<li><a href="/app">Catalog</a></li><li><a href="/search">Search</a></li><li><a href="/create">Create Album</a></li><li><a href="/logout">Logout</a></li>'
+        : authUl.innerHTML = '<li><a href="/app">Catalog</a></li><li><a href="/search">Search</a></li><li><a href="/login">Login</a></li><li><a href="/register">Register</a></li>';
+}
+
+export function decorateCTX(ctx, next) {
+    const main = document.getElementsByTagName('main')[0];
+
+    ctx.render = function (content) {
+        return render(content, main);
+    }
+    ctx.setNavigation = setNavigation;
+    ctx.userData = getUserData();
+    next();
+}
+
+export function guardRoute(status) {
+    return function (ctx, next) {
+        const user = getUserData();
+        const isUser = Boolean(user);
+
+        if ((status === 'user' && isUser) || (status === 'guest' && !isUser)) next();
+        else page.redirect('/');
+    };
+}
