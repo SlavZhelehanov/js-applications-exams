246070f1919eb94580e02b156ca2c92eca38fbdb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-24 19:14:55 +0200

    Add "Register Page"

--- 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/app.js
+++ 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/app.js
@@ -1,8 +1,9 @@
 import page from "./lib/page.mjs";
 
-import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
-import { homePage } from "./views/home/home.js";
-import { dashboardPage } from "./views/app/dashboard.js";
+import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
+import {homePage} from "./views/home/home.js";
+import {dashboardPage} from "./views/app/dashboard.js";
+import {registerPage} from "./views/auth/register";
 
 setNavigation();
 
@@ -10,5 +11,6 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/app", dashboardPage);
+page("/register", guardRoute("guest"), registerPage);
 
 page.start();
