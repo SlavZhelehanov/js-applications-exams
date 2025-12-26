f9e8472180462f0cc81650b7fcb6c599528468cf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-26 19:48:55 +0200

    Login User

--- 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/views/auth/login.js
+++ 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/views/auth/login.js
@@ -1,9 +1,11 @@
 import {html} from '../../lib/lit-html.min.js';
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onLogin) {
     return html`
         <section id="loginPage">
-            <form>
+            <form @submit=${onLogin}>
                 <fieldset>
                     <legend>Login</legend>
 
@@ -16,7 +18,7 @@ function template() {
                     <button type="submit" class="login">Login</button>
 
                     <p class="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
+                        <span>If you don't have profile click <a href="/register">here</a></span>
                     </p>
                 </fieldset>
             </form>
@@ -24,5 +26,27 @@ function template() {
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email.trim() === '' || password.trim() === '') return alert('All fields are required!');
+
+        try {
+            const user = await post("/users/login", {email, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
