40910074fc3da336a419008935934827cb184314
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-28 19:38:18 +0200

    Edit Album

--- 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/views/app/edit.js
+++ 15. JS Applications Regular Exam - 4 December 2021 - Music App/resources/src/views/app/edit.js
@@ -1,33 +1,35 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get, put} from "../../utils/api.js";
 
-function template() {
+function template(item, onEdit) {
     return html`
         <section class="editPage">
-            <form>
+            <form @submit=${onEdit}>
                 <fieldset>
                     <legend>Edit Album</legend>
                     <div class="container">
                         <label for="name" class="vhide">Album name</label>
-                        <input id="name" name="name" class="name" type="text" value="In These Silent Days">
+                        <input id="name" name="name" class="name" type="text" value=${item.name}>
 
                         <label for="imgUrl" class="vhide">Image Url</label>
-                        <input id="imgUrl" name="imgUrl" class="imgUrl" type="text" value="./img/BrandiCarlile.png">
+                        <input id="imgUrl" name="imgUrl" class="imgUrl" type="text" value=${item.imgUrl}>
 
                         <label for="price" class="vhide">Price</label>
-                        <input id="price" name="price" class="price" type="text" value="12.80">
+                        <input id="price" name="price" class="price" type="text" value=${item.price}>
 
                         <label for="releaseDate" class="vhide">Release date</label>
-                        <input id="releaseDate" name="releaseDate" class="releaseDate" type="text" value="October 1, 2021">
+                        <input id="releaseDate" name="releaseDate" class="releaseDate" type="text"
+                               value=${item.releaseDate}>
 
                         <label for="artist" class="vhide">Artist</label>
-                        <input id="artist" name="artist" class="artist" type="text" value="Brandi Carlile">
+                        <input id="artist" name="artist" class="artist" type="text" value=${item.artist}>
 
                         <label for="genre" class="vhide">Genre</label>
-                        <input id="genre" name="genre" class="genre" type="text" value="Low Country Sound Music">
+                        <input id="genre" name="genre" class="genre" type="text" value=${item.genre}>
 
                         <label for="description" class="vhide">Description</label>
                         <textarea name="description" class="description" rows="10"
-                            cols="10">Upon release, In These Silent Days received positive reviews from critics. At Metacritic, which assigns a normalized rating out of 100 to reviews from mainstream critics, the album has an average score of 87 out of 100, which indicates 'universal acclaim'.</textarea>
+                                  cols="10">${item.description}</textarea>
 
                         <button class="edit-album" type="submit">Edit Album</button>
                     </div>
@@ -37,5 +39,39 @@ function template() {
 }
 
 export async function editPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    async function onEdit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const newItem = {
+            name: formData.get('name').trim(),
+            imgUrl: formData.get('imgUrl').trim(),
+            price: formData.get('price').trim(),
+            releaseDate: formData.get('releaseDate').trim(),
+            artist: formData.get('artist').trim(),
+            genre: formData.get('genre').trim(),
+            description: formData.get('description').trim()
+        }
+
+        if (Object.values(newItem).some((x) => !x)) return alert("All fields are required!");
+
+        try {
+            await put(`/data/albums/${id}`, newItem);
+            e.target.reset();
+            ctx.page.redirect(`/details/${id}`);
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+    try {
+        item = await get(`/data/albums/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(item, onEdit));
 }
