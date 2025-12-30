7549a1dea5f43b422d4927835528221becb66935
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-30 17:23:56 +0200

    Set App Navigation

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
@@ -2,6 +2,8 @@ import page from "./lib/page.mjs";
 
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 
+setNavigation();
+
 page(decorateCTX);
 page("/index.html", "/");
 
---
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/utils/utils.js
@@ -0,0 +1,47 @@
+import {render} from "../lib/lit-html.min.js";
+import page from "../lib/page.mjs";
+
+const item = "userData";
+
+export function saveUserData(data) {
+    sessionStorage.setItem(item, JSON.stringify(data));
+}
+
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem(item));
+}
+
+export function clearUserData() {
+    sessionStorage.removeItem(item);
+}
+
+export function setNavigation() {
+    const isLoggedIn = Boolean(getUserData());
+    const authDiv = document.getElementsByTagName('nav')[0].getElementsByTagName('div')[0];
+    const username = getUserData() ? getUserData().username : null;
+
+    isLoggedIn
+        ? [authDiv.id = 'profile', authDiv.innerHTML = `<a>Welcome ${username}</a><a href="/my-listings">My Listings</a><a href="/create">Create Listing</a><a href="/logout">Logout</a>`]
+        : [authDiv.id = 'guest', authDiv.innerHTML = '<a href="/login">Login</a><a href="/register">Register</a>'];
+}
+
+export function decorateCTX(ctx, next) {
+    const main = document.getElementsByTagName('main')[0];
+
+    ctx.render = function (content) {
+        return render(content, main);
+    }
+    ctx.setNavigation = setNavigation;
+    ctx.userData = getUserData();
+    next();
+}
+
+export function guardRoute(status) {
+    return function (ctx, next) {
+        const user = getUserData();
+        const isUser = Boolean(user);
+
+        if ((status === 'user' && isUser) || (status === 'guest' && !isUser)) next();
+        else page.redirect('/');
+    };
+}
