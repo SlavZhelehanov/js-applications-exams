a4585be206fbcb73452797433bd6af6c15a4b482
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-12-31 16:38:02 +0200

    Add hard coded "All Listings Page"

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
@@ -2,11 +2,13 @@ import page from "./lib/page.mjs";
 
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
+import {dashboardPage} from "./views/app/dashboard.js";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
+page("/app", dashboardPage);
 
 page.start();
