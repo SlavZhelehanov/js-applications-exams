8f77a3dddf66e8b3e366700f07d7140b2372234a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-01 19:08:15 +0200

    Add "Register Page"

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
@@ -1,9 +1,10 @@
 import page from "./lib/page.mjs";
 
-import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
+import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard.js";
 import {loginPage} from "./views/auth/login.js";
+import {registerPage} from "./views/auth/register.js";
 
 setNavigation();
 
@@ -11,6 +12,7 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/app", dashboardPage);
-page("/login", loginPage);
+page("/login", guardRoute("guest"), loginPage);
+page("/register", guardRoute("guest"), registerPage);
 
 page.start();
