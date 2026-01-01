82cfea128013b1cea24b8a403ba6a67b00a39a83
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-01 19:06:31 +0200

    Add "Login Page"

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
@@ -3,6 +3,7 @@ import page from "./lib/page.mjs";
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 
@@ -10,5 +11,6 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/app", dashboardPage);
+page("/login", loginPage);
 
 page.start();
