1e479557bf2de3c2d9867018c6f3e8bd6b74353c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-03 17:40:49 +0200

    Add hard coded "My Listings Page"

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/app.js
@@ -8,6 +8,7 @@ import {registerPage} from "./views/auth/register.js";
 import {createPage} from "./views/app/create.js";
 import {detailsPage} from "./views/app/details.js";
 import {editPage} from "./views/app/edit.js";
+import {myListingsPage} from "./views/app/my-listings.js";
 
 setNavigation();
 
@@ -20,5 +21,6 @@ page("/register", guardRoute("guest"), registerPage);
 page("/create", guardRoute("user"), createPage);
 page("/details/:id", detailsPage);
 page("/edit/:id", guardRoute("user"), editPage);
+page("/my-listings", guardRoute("user"), myListingsPage);
 
 page.start();
