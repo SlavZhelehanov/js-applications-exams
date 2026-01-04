21293818f17a4756de30377169179b0f007ab5f5
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-04 19:19:18 +0200

    Show correct data in "Dashboard/Catalog Page"

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/app/dashboard.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/app/dashboard.js
@@ -1,66 +1,42 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <section id="car-listings">
             <h1>Car Listings</h1>
             <div class="listings">
-
-                <!-- Display all records -->
-                <div class="listing">
+                ${0 < data.length
+                        ? data.map(g => html`<div class="listing">
                     <div class="preview">
-                        <img src="/images/audia3.jpg">
+                        <img src=${g.imageUrl}>
                     </div>
-                    <h2>Audi A3</h2>
+                    <h2>${g.brand} ${g.model}</h2>
                     <div class="info">
                         <div class="data-info">
-                            <h3>Year: 2018</h3>
-                            <h3>Price: 25000 $</h3>
+                            <h3>Year: ${g.year}</h3>
+                            <h3>Price: ${g.price} $</h3>
                         </div>
                         <div class="data-buttons">
-                            <a href="#" class="button-carDetails">Details</a>
+                            <a href="/details/${g._id}" class="button-carDetails">Details</a>
                         </div>
                     </div>
-                </div>
-
-                <div class="listing">
-                    <div class="preview">
-                        <img src="/images/benz.jpg">
-                    </div>
-                    <h2>Mercedes A-class</h2>
-                    <div class="info">
-                        <div class="data-info">
-                            <h3>Year: 2016</h3>
-                            <h3>Price: 27000 $</h3>
-                        </div>
-                        <div class="data-buttons">
-                            <a href="#" class="button-carDetails">Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="listing">
-                    <div class="preview">
-                        <img src="/images/bmw.jpg">
-                    </div>
-                    <h2>BMW 3 series</h2>
-                    <div class="info">
-                        <div class="data-info">
-                            <h3>Year: 2016</h3>
-                            <h3>Price: 22000 $</h3>
-                        </div>
-                        <div class="data-buttons">
-                            <a href="#" class="button-carDetails">Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Display if there are no records -->
-                <p class="no-cars">No cars in database.</p>
+                </div>`
+                        )
+                        : html`<p class="no-cars">No cars in database.</p>`
+                }
             </div>
         </section>`;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get("/data/cars?sortBy=_createdOn%20desc");
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(data));
 }
