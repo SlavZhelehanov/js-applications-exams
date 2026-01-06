24c96b86c9c8843b7e6c1ed7934a11330d073f61
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-06 13:55:28 +0200

    Create Car Listing

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/app/create.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/app/create.js
@@ -1,10 +1,11 @@
 import {html} from "../../lib/lit-html.min.js";
+import {post} from "../../utils/api.js";
 
-function template() {
+function template(onCreate) {
     return html`
         <section id="create-listing">
             <div class="container">
-                <form id="create-form">
+                <form id="create-form" @submit=${onCreate}>
                     <h1>Create Car Listing</h1>
                     <p>Please fill in this form to create an listing.</p>
                     <hr>
@@ -35,5 +36,29 @@ function template() {
 }
 
 export async function createPage(ctx) {
-    ctx.render(template());
+    async function onCreate(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const item = {
+            brand: formData.get('brand').trim(),
+            model: formData.get('model').trim(),
+            description: formData.get('description').trim(),
+            year: +formData.get('year').trim(),
+            imageUrl: formData.get('imageUrl').trim(),
+            price: +formData.get('price').trim()
+        }
+
+        if (Object.values(item).some((x) => !x)) return alert("All fields are required!");
+
+        try {
+            await post("/data/cars", item);
+            e.target.reset();
+            ctx.page.redirect('/app');
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+    ctx.render(template(onCreate));
 }
