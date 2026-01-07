cf699283bdf0544a9ee6725d38c8e58c60255432
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-07 12:07:44 +0200

    Search car by year

--- 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/home/search.js
+++ 16. JS Applications Exam Preparation 2 - Cars Tube/resources/src/views/home/search.js
@@ -1,41 +1,60 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(onSearch, data) {
     return html`
         <section id="search-cars">
             <h1>Filter by year</h1>
 
             <div class="container">
                 <input id="search-input" type="text" name="search" placeholder="Enter desired production year">
-                <button class="button-list">Search</button>
+                <button @click=${onSearch} class="button-list">Search</button>
             </div>
 
             <h2>Results:</h2>
             <div class="listings">
-
-                <!-- Display all records -->
-                <div class="listing">
+                ${0 < data.length
+                        ? data.map(s => html`<div class="listing">
                     <div class="preview">
-                        <img src="/images/audia3.jpg">
+                        <img src=${s.imageUrl}>
                     </div>
-                    <h2>Audi A3</h2>
+                    <h2>${s.brand} ${s.model}</h2>
                     <div class="info">
                         <div class="data-info">
-                            <h3>Year: 2018</h3>
-                            <h3>Price: 25000 $</h3>
+                            <h3>Year: ${s.year}</h3>
+                            <h3>Price: ${s.price} $</h3>
                         </div>
                         <div class="data-buttons">
-                            <a href="#" class="button-carDetails">Details</a>
+                            <a href="/details/${s._id}" class="button-carDetails">Details</a>
                         </div>
                     </div>
-                </div>
-
-                <!-- Display if there are no matches -->
-                <p class="no-cars"> No results.</p>
+                </div>`)
+                        : html`<p class="no-cars"> No results.</p>`
+                }
             </div>
         </section>`;
 }
 
 export async function searchPage(ctx) {
-    ctx.render(template());
+    const item = ctx.querystring.split('=')[1];
+    let data = '';
+
+    try {
+        if (item && 0 < item.length) data = await get(`/data/cars?where=year%3D${item}`);
+        console.log(data);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    async function onSearch(e) {
+        e.preventDefault();
+
+        const query = document.getElementById('search-input').value;
+
+        if (query.trim() === '') return alert('All fields are required!');
+
+        ctx.page.redirect(`/search?query=${query}`);
+    }
+
+    ctx.render(template(onSearch, data));
 }
