b57e49f2dc4eaac8f4597f07d2a3cfe77e3207e4
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-12 18:34:05 +0200

    Register User

--- 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/auth/register.js
+++ 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/auth/register.js
@@ -1,12 +1,14 @@
 import { html } from "../../lib/lit-html.min.js";
+import { post } from "../../utils/api.js";
+import { saveUserData, showError } from "../../utils/utils.js";
 
-function template() {
+function template(onRegister) {
     return html`
         <section id="registerView">
             <div class="background-spotify">
                 <div class="song-container">
                     <h1>Register</h1>
-                    <form action="#" method="POST">
+                    <form @submit=${onRegister}>
                         <div class="form-group">
                             <label for="username" class="white-labels">Username</label>
                             <input type="text" name="username" class="form-control" placeholder="Enter username">
@@ -24,5 +26,29 @@ function template() {
 }
 
 export function registerPage(ctx) {
-    ctx.render(template());
+    async function onRegister(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        // if (email === '' || password === '') return showError('All fields are required');
+        // if (password !== repass) return showError("Passwords don't match");
+
+        try {
+            const user = await post("/users/register", { username, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/app');
+        } catch (err) {
+            if (err.message) showError(err.message);
+            else showError(err);
+        }
+    }
+
+    ctx.render(template(onRegister));
 }
