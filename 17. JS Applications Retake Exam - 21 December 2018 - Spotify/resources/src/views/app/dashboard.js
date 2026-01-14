3bedc8f7622f2a71512339158be898946ca6de2d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-14 16:28:36 +0200

    Show Dynamic data in "Dashboard Page"

--- 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/app/dashboard.js
+++ 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/app/dashboard.js
@@ -1,14 +1,28 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <section id="allSongsView">
             <div class="background-spotify">
                 <div class="song-container">
                     <h1>All Songs</h1>
-                    <a href="#">
+                    <a href="/create">
                         <button type="button" class="btn-lg btn-block new-song-btn">Add a new song</button>
                     </a>
+                    ${0 < data.length
+                            ? data.map(m => html`<div class="song">
+                        <h5>Title: ${m.title}</h5>
+                        <h5>Artist: ${m.artist}</h5>
+                        <img class="cover" src=${m.imageUrl}/>
+
+                        <p>Likes: 100; Listened 1500 times</p>
+                        <a href="#"><button type="button" class="btn btn-danger mt-4">Remove</button></a>
+                        <a href="#"><button type="button" class="btn btn-success mt-4">Listen</button></a>
+                        <p>Likes: 100</p>
+                        <a href="#"><button type="button" class="btn btn-primary mt-4">Like</button></a>`)
+                        : null
+                    }
                     <div class="song">
                         <h5>Title: When The Sun Goes Down</h5>
                         <h5>Artist: Zeni N, The Distance, Igi</h5>
@@ -45,5 +59,13 @@ function template() {
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get('/data/songs?sortBy=_createdOn%20desc');
+    } catch (err) {
+        console.log(err.message);
+    }
+
+    ctx.render(template(data));
 }
