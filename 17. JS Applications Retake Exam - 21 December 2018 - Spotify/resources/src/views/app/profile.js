666dc9985d839777623271b4b02bed457e50c61d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-17 17:13:26 +0200

    Show correct data in "My Profile Page"

--- 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/app/profile.js
+++ 17. JS Applications Retake Exam - 21 December 2018 - Spotify/resources/src/views/app/profile.js
@@ -1,11 +1,25 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(songs) {
     return html`
         <section id="mySongsView">
             <div class="background-spotify">
                 <div class="song-container">
                     <h1>My Songs</h1>
+                    ${0 < songs.length
+                            ? songs.map(m => html`<div class="song">
+                        <h5>Title: ${m.title}</h5>
+                        <h5>Artist: ${m.artist}</h5>
+                        <img class="cover" src=${m.imageUrl}/>
+                        <p>Likes: ${m.likes}; Listened ${m.listened} times</p>
+                        <a href="/delete/${m._id}"><button type="button" class="btn btn-danger mt-4">Remove</button></a>
+                        <a href="/listen/${m._id}"><button type="button" class="btn btn-success mt-4">Listen</button></a>
+                        <p>Likes: 100</p>
+                        <a href="/like/${m._id}"><button type="button" class="btn btn-primary mt-4">Like</button></a>
+                    </div>`)
+                            : null
+                    }
                     <div class="song">
                         <h5>Title: When The Sun Goes Down</h5>
                         <h5>Artist: Zeni N, The Distance, Igi</h5>
@@ -38,9 +52,17 @@ function template() {
                     </div>
                 </div>
             </div>
-        </section>`;
+        </section>`
 }
 
-export async function profilePage(ctx) {
-    ctx.render(template());
+export async function myProfilePage(ctx) {
+    let songs = [];
+
+    try {
+        songs = await get(`/data/songs?where=_ownerId%3D%22${ctx.userData._id}%22&sortBy=_createdOn%20desc`);
+    } catch (err) {
+        showError(err.message);
+    }
+
+    ctx.render(template(songs));
 }
