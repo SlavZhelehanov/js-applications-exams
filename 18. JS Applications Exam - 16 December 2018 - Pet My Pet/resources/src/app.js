cbc290f22a4383e46f9097103ed8f2533c5ae259
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-22 16:20:10 +0200

    Add "Login Page"

--- 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/app.js
+++ 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/app.js
@@ -3,6 +3,7 @@ import page from "./lib/page.mjs";
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 
@@ -10,5 +11,6 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
+page("/login", guardRoute("guest"), loginPage);
 
 page.start();
