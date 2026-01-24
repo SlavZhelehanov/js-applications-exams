45b5f113033e8c0105bd956b4ea5d540af692507
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-24 21:26:25 +0200

    Add hard coded "Details Page"

--- 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/app.js
+++ 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/app.js
@@ -6,6 +6,7 @@ import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
 import {createPage} from "./views/app/create.js";
 import {dashboardPage} from "./views/app/dashboard.js";
+import {detailsPage} from "./views/app/details.js";
 
 setNavigation();
 
@@ -16,4 +17,5 @@ page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/create", guardRoute("user"), createPage);
 page("/dashboard", guardRoute("user"), dashboardPage);
+page("/details/:id", guardRoute("user"), detailsPage);
 page.start();
