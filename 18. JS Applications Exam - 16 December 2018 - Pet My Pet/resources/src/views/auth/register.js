0a6f7c5819229e8142d99e03c540f06b0a654e59
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-27 15:30:41 +0200

    Register User

--- 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/auth/register.js
+++ 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/auth/register.js
@@ -1,9 +1,11 @@
 import { html } from "../../lib/lit-html.min.js";
+import { post } from "../../utils/api.js";
+import { saveUserData, showMessage } from "../../utils/utils.js";
 
-function template() {
+function template(onRegister) {
     return html`
         <section class="register">
-            <form action="#" method="post">
+            <form @submit=${onRegister}>
                 <fieldset>
                     <legend>Register</legend>
                     <p class="field">
@@ -29,5 +31,31 @@ function template() {
 }
 
 export function registerPage(ctx) {
-    ctx.render(template());
+    async function onRegister(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        if (username.length < 3) return showMessage( "err",'The username should be at least 3 symbols');
+        if (password.length < 6) return showMessage( "err",'The password should be at least 6 symbols');
+
+        try {
+            showMessage("loading", 'Loading...');
+            const user = await post("/users/register", { username, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            await showMessage("info", 'User registration successful.');
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            if (err.message) showMessage("err", err.message);
+            else showMessage("err", err);
+        }
+    }
+
+    ctx.render(template(onRegister));
 }
