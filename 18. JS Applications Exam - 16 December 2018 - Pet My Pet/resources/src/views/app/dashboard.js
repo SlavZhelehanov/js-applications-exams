b8781a2b8c7cb17455700a86c41e0358f5daaf93
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-29 18:04:40 +0200

    pet a Pet

--- 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/app/dashboard.js
+++ 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/app/dashboard.js
@@ -1,8 +1,8 @@
 import {html} from "../../lib/lit-html.min.js";
-import {get} from "../../utils/api.js";
+import {get, post} from "../../utils/api.js";
 import {showMessage} from "../../utils/utils.js";
 
-function template({data, onDetails, isAuth, showAll}) {
+function template({data, onDetails, onPet, isAuth, showAll}) {
     return html`
         <section class="dashboard">
             <h1>Dashboard</h1>
@@ -28,13 +28,8 @@ function template({data, onDetails, isAuth, showAll}) {
                                     ${!isAuth._id
                                             ? null
                                             : isAuth._id !== pet._ownerId
-                                            ? html`
-                                                <a href="#">
-                                                    <button class="button"><i class="fas fa-heart"></i> Pet</button>
-                                                </a>
-                                                <a href="#">
-                                                    <button class="button">Details</button>
-                                                </a>`
+                                            ? html`<a><button @click=${() => onPet(m._id)} class="button"><i class="fas fa-heart"></i> Pet</button></a>
+                                                    <a><button @click=${() => onDetails(m._id)} class="button">Details</button></a>`
                                                     : html`<a><button @click=${() => onDetails(m._id)} class="button">Details</button></a>`
                                     }
                                     <i class="fas fa-heart"></i> <span> ${pet.likes}</span>
@@ -59,6 +54,17 @@ export async function dashboardPage(ctx) {
         ctx.page.redirect(`/details/${id}`);
     }
 
+    async function onPet(id) {
+        try {
+            await post(`/data/likes`, { petId: id });
+            showMessage("info", "Liked!");
+            ctx.page.redirect('/app');
+        } catch (err) {
+            if (err.message) showMessage("err", err.message);
+            else showMessage("err", err);
+        }
+    }
+
     try {
         data = await get('/data/pets?sortBy=_createdOn%20desc');
 
@@ -68,5 +74,5 @@ export async function dashboardPage(ctx) {
         else showMessage("err", err);
     }
 
-    ctx.render(template({data, onDetails, isAuth, showAll}));
+    ctx.render(template({onPet, data, onDetails, isAuth, showAll}));
 }
