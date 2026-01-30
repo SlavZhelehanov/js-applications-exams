c11eb8ca5ad1882af66bbd5088399b7a7134e151
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-01-30 18:55:49 +0200

    Show correct data in "My Pets Page"

--- 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/app/profile.js
+++ 18. JS Applications Exam - 16 December 2018 - Pet My Pet/resources/src/views/app/profile.js
@@ -1,9 +1,31 @@
 import {html} from '../../lib/lit-html.min.js';
+import { get } from "../../utils/api.js";
+import { showMessage } from "../../utils/utils.js";
 
-function template() {
+function template({pets}) {
     return html`
         <section class="my-pets">
             <h1>My Pets</h1>
+            ${0 < pets.length 
+                    ? html`<ul class="my-pets-list">
+                        ${pets.map(pet => html`<section class="myPet">
+                        <h3>Name: ${pet.name}</h3>
+                        <p>Category: ${pet.category}</p>
+                        <p class="img"><img src=${pet.imageURL}></p>
+                        <p class="description">${pet.description}</p>
+                        <div class="pet-info">
+                            <a href="/details/${pet._id}">
+                                <button class="button">Details</button>
+                            </a>
+                            <a href="#">
+                                <button class="button">Delete</button>
+                            </a>
+                            <i class="fas fa-heart"></i> <span>${pet.likes}</span>
+                        </div>
+                    </section>`)}
+                    </ul>` 
+                    : null
+    }
             <ul class="my-pets-list">
                 <section class="myPet">
                     <h3>Name: Pesho</h3>
@@ -55,5 +77,14 @@ function template() {
 }
 
 export async function myProfilePage(ctx) {
-    ctx.render(template());
+    let pets = [];
+
+    try {
+        pets = await get(`/data/pets?where=_ownerId%3D%22${ctx.userData._id}%22&sortBy=_createdOn%20desc`);
+    } catch (err) {
+        if (err.message) showMessage("err", err.message);
+        else showMessage("err", err);
+    }
+
+    ctx.render(template({pets}));
 }
