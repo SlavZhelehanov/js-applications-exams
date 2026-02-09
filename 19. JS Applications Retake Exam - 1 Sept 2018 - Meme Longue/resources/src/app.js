2ef78aa9f6d95872b60643eba15428a7054ffe97
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-09 20:44:50 +0200

    Add "Register Page"

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/app.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/app.js
@@ -1,8 +1,9 @@
 import page from "./lib/page.js";
 
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
-import {homePage} from "./views/home/home";
-import {loginPage} from "./views/auth/login";
+import {homePage} from "./views/home/home.js";
+import {loginPage} from "./views/auth/login.js";
+import {registerPage} from "./views/auth/register.js";
 
 setNavigation();
 
@@ -10,5 +11,6 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/login", guardRoute("guest"), loginPage);
+page("/register", guardRoute("guest"), registerPage);
 
 page.start();
