476882a592aedcdc4a5bba9fbcee0ad4890e7cd2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-11 16:11:09 +0200

    Login User

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/auth/login.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/auth/login.js
@@ -1,16 +1,18 @@
 import { html } from '../../lib/lit-html.min.js';
+import { post } from "../../utils/api.js";
+import { saveUserData } from "../../utils/utils.js";
 
-function template() {
+function template(onLogin) {
     return html`
         <div id="login">
-            <form action="#" method="post">
+            <form  @submit=${onLogin}>
                 <div class="container">
                     <h1>Login</h1>
                     <p id="details">Please enter your credentials.</p>
                     <hr id="login-register-hr">
 
-                    <p id="username">Username</p>
-                    <input placeholder="Enter Username" name="username" type="text">
+                    <p id="username">Email</p>
+                    <input placeholder="Enter Email" name="email" type="email">
 
                     <p>Password</p>
                     <input type="password" placeholder="Enter Password" name="password">
@@ -19,13 +21,35 @@ function template() {
 
                 <div class="container signin">
                     <p>Dont have an account?
-                        <a href="#">Sign up</a>.</p>
+                        <a href="/register">Sign up</a>.</p>
                 </div>
             </form>
         </div>`;
 }
 
 export function loginPage(ctx) {
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
 
-    ctx.render(template());
+        if (email.trim() === '' || password.trim() === '') return alert('All fields are required!');
+
+        try {
+            const user = await post("/users/login", { email, password });
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/app');
+        } catch (err) {
+            if (err.message) alert(err.message);
+            else alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
