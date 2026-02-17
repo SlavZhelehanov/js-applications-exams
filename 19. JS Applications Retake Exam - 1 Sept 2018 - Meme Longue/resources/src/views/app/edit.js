c1d9d24ffad06cb7c6ab6085c379cb976dcdb434
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-17 14:34:48 +0200

    Edit Meme data

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
@@ -1,10 +1,10 @@
 import { html } from '../../lib/lit-html.min.js';
 import { get, put } from "../../utils/api.js";
 
-function template(item) {
+function template(item, onEdit) {
     return html`
         <div id="edit-meme">
-            <form >
+            <form @submit=${onEdit}>
                 <h1>Edit Meme</h1>
                 <div class="container">
                     <p>Please fill in this form to edit an meme.</p>
@@ -28,11 +28,28 @@ export async function editPage(ctx) {
     const id = ctx.params.id;
     let item = {};
 
+    async function onEdit(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const newItem = {
+            title: formData.get('title').trim(),
+            description: formData.get('description').trim(),
+            imageUrl: formData.get('imageUrl').trim()
+        }
+
+        if (Object.values(newItem).some((x) => !x)) return alert("All fields are required!");
+
+        await put(`/data/memes/${id}`, newItem);
+        e.target.reset();
+        ctx.page.redirect(`/details/${id}`);
+    }
+
     try {
         item = await get(`/data/memes/${id}`);
     } catch (err) {
         alert(err.message);
     }
 
-    ctx.render(template(item));
+    ctx.render(template(item, onEdit));
 }
