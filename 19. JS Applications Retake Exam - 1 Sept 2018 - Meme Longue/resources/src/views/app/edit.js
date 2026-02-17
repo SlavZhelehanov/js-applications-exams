979e3a7732412ab9bf5c7904c5fb14874b73b915
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-17 14:34:11 +0200

    Populate Edit form with correct data

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
@@ -1,21 +1,22 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get, put } from "../../utils/api.js";
 
-function template(item, onEdit) {
+function template(item) {
     return html`
         <div id="edit-meme">
-            <form>
+            <form >
                 <h1>Edit Meme</h1>
                 <div class="container">
                     <p>Please fill in this form to edit an meme.</p>
 
                     <p>Title</p>
-                    <input type="text" placeholder="Enter Title" name="title" value="Title">
+                    <input type="text" placeholder="Enter Title" name="title" value=${item.title}>
 
                     <p>Description</p>
-                    <input type="text" placeholder="Enter Description" name="description" value="Description">
+                    <input type="text" placeholder="Enter Description" name="description" value=${item.description}>
 
                     <p>Meme Image</p>
-                    <input type="text" placeholder="Enter Meme ImageUrl" name="imageUrl" value="ImageUrl">
+                    <input type="text" placeholder="Enter Meme ImageUrl" name="imageUrl" value=${item.imageUrl}>
 
                     <button type="submit" class="registerbtn">Edit Meme</button>
                 </div>
@@ -24,5 +25,14 @@ function template(item, onEdit) {
 }
 
 export async function editPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    try {
+        item = await get(`/data/memes/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(item));
 }
