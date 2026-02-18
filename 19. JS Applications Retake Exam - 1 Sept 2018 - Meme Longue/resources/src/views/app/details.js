93b339beb167dee7e197911dda8e8f647b95f215
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-18 22:19:23 +0200

    Show correct Meme data in "Details Page"

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/details.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/details.js
@@ -1,20 +1,19 @@
 import { html } from '../../lib/lit-html.min.js';
+import { get } from "../../utils/api.js";
 
-function template() {
+function template({item}) {
     return html`
         <div class="meme-details">
             <div class="my-meme-details">
-                <a href="#" id="meme-title">Разбираемо...</a>
-                <img src="https://scontent.fsof3-1.fna.fbcdn.net/v/t1.0-9/31924539_2110321352572189_5652160133854134272_n.jpg?_nc_cat=0&oh=dc73aad9af411f76454dd70ea1ab2775&oe=5C2FB35A">
+                <a href="#" id="meme-title">${item.title}</a>
+                <img src=${item.imageUrl}>
                 <div class="meme-props">
                     <h2>Description</h2>
-                    <p class="meme-description">
-                        И аз така...
-                    </p>
+                    <p class="meme-description">${item.description}</p>
                 </div>
 
                 <div class="meme-details-buttons">
-                    <a class="meme-details-button" href="#">Created by User</a>
+                    <a class="meme-details-button" href="#">Created by ${item.creator}</a>
                     <a href="#" class="meme-details-button">Edit</a>
                     <a href="#" class="meme-details-button">Delete</a>
                 </div>
@@ -24,5 +23,14 @@ function template() {
 }
 
 export async function detailsPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    try {
+        item = await get(`/data/memes/${id}`);
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template({item}));
 }
