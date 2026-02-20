f0691533282ffe066f203b3335cf4b793961a77f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-20 17:18:34 +0200

    Show Meme data in "Dashboard Page"

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/dashboard.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/dashboard.js
@@ -1,32 +1,40 @@
 import { html } from "../../lib/lit-html.min.js";
+import { get } from "../../utils/api.js";
 
-function template() {
+function template(data) {
     return html`
         <div id="meme-feed">
             <h1>Meme Feed</h1>
             <div id="memes">
-                <div class="meme">
-                    <a href="#" class="meme-title">Разбираемо...</a>
+                ${0 < data.length
+                        ? data.map(m => html`<div class="meme">
+                    <a href="#" class="meme-title">${m.title}</a>
                     <br>
-                    <a href="#"><img class="meme-image" src="https://scontent.fsof3-1.fna.fbcdn.net/v/t1.0-9/31924539_2110321352572189_5652160133854134272_n.jpg?_nc_cat=0&oh=dc73aad9af411f76454dd70ea1ab2775&oe=5C2FB35A"></a>
+                    <a href="#"><img class="meme-image" src=${m.imageUrl}></a>
                     <div class="info">
-
                         <div id="data-buttons">
                             <a href="#" class="custom-button">Check Out</a>
-                            <a href="#" class="custom-button">Edit</a>
+                            <a href="/edit/${m._id}" class="custom-button">Edit</a>
                             <a href="#" class="custom-button">Delete</a>
-                            <a href="#" class="creator">Creator: User</a>
+                            <a href="#" class="creator">Creator: ${m.creator}</a>
                         </div>
                     </div>
                     <hr>
-                </div>
-
-                <p class="no-memes">No memes in database.</p>
-
+                </div>`)
+                        : html`<p class="no-memes">No memes in database.</p>`
+                }
             </div>
         </div>`;
 }
 
 export async function dashboardPage(ctx) {
-    ctx.render(template());
+    let data = [];
+
+    try {
+        data = await get('/data/memes?sortBy=_createdOn%20desc');
+    } catch (err) {
+        alert(err.message);
+    }
+
+    ctx.render(template(data));
 }
