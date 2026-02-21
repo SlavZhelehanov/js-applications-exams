f2186aa65163c69070bbfe424cd2babcb99466f6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-21 17:47:46 +0200

    Show "Edit" and "Delete" only to meme's creator

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/dashboard.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/dashboard.js
@@ -1,26 +1,34 @@
-import { html } from "../../lib/lit-html.min.js";
-import { get, del } from "../../utils/api.js";
+import {html} from "../../lib/lit-html.min.js";
+import {get, del} from "../../utils/api.js";
 
-function template({data, onDelete}) {
+function template({data, onDelete, isAuth}) {
     return html`
         <div id="meme-feed">
             <h1>Meme Feed</h1>
             <div id="memes">
                 ${0 < data.length
-                        ? data.map(m => html`<div class="meme">
-                    <a href="#" class="meme-title">${m.title}</a>
-                    <br>
-                    <a href="#"><img class="meme-image" src=${m.imageUrl}></a>
-                    <div class="info">
-                        <div id="data-buttons">
-                            <a href="#" class="custom-button">Check Out</a>
-                            <a href="/edit/${m._id}" class="custom-button">Edit</a>
-                            <a @click=${() => onDelete(m._id)} href="javascript:void(0)" class="custom-button">Delete</a>
-                            <a href="#" class="creator">Creator: ${m.creator}</a>
-                        </div>
-                    </div>
-                    <hr>
-                </div>`)
+                        ? data.map(m => html`
+                            <div class="meme">
+                                <a href="#" class="meme-title">${m.title}</a>
+                                <br>
+                                <a href="#"><img class="meme-image" src=${m.imageUrl}></a>
+                                <div class="info">
+                                    <div id="data-buttons">
+                                        <a href="#" class="custom-button">Check Out</a>
+                                        ${isAuth
+                                                ? html`<a href="/edit/${m._id}" class="custom-button">Edit</a>
+                                                <a @click=${() => onDelete(m._id)} href="javascript:void(0)"
+                                                   class="custom-button">Delete</a>`
+                                                : null
+                                        }
+                                        <a href="/edit/${m._id}" class="custom-button">Edit</a>
+                                        <a @click=${() => onDelete(m._id)} href="javascript:void(0)"
+                                           class="custom-button">Delete</a>
+                                        <a href="#" class="creator">Creator: ${m.creator}</a>
+                                    </div>
+                                </div>
+                                <hr>
+                            </div>`)
                         : html`<p class="no-memes">No memes in database.</p>`
                 }
             </div>
@@ -28,6 +36,7 @@ function template({data, onDelete}) {
 }
 
 export async function dashboardPage(ctx) {
+    const isAuth = !!ctx.userData;
     let data = [];
 
     async function onDelete(id) {
@@ -49,5 +58,5 @@ export async function dashboardPage(ctx) {
         alert(err.message);
     }
 
-    ctx.render(template({data, onDelete}));
+    ctx.render(template({data, isAuth, onDelete}));
 }
