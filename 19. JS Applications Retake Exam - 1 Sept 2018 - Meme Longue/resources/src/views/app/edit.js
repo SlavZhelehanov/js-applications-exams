c760ec87cc68c8fa77e12d8be81966586ef154b7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-28 18:14:32 +0200

    Add form validation and proper messages during meme editing

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/edit.js
@@ -1,5 +1,6 @@
-import { html } from '../../lib/lit-html.min.js';
-import { get, put } from "../../utils/api.js";
+import {html} from '../../lib/lit-html.min.js';
+import {get, put} from "../../utils/api.js";
+import {showMessage} from "../../utils/utils.js";
 
 function template(item, onEdit) {
     return html`
@@ -38,9 +39,14 @@ export async function editPage(ctx) {
             imageUrl: formData.get('imageUrl').trim()
         }
 
-        if (Object.values(newItem).some((x) => !x)) return alert("All fields are required!");
+        if (Object.values(item).some((x) => !x)) return showMessage("errorBox", "All fields are required!");
+        if (33 < item.title.length) return showMessage("errorBox", "The title length must not exceed 33 characters!");
+        if (item.description.length < 30 || 450 < item.description.length) return showMessage("errorBox", "The description length must not exceed 450 characters and should be at least 30!");
+        if (!item.imageUrl.startsWith("http://") && !item.imageUrl.startsWith("https://")) return showMessage("errorBox", "Link url should always start with “http”.");
 
+        showMessage("loadingBox", "Loading...");
         await put(`/data/memes/${id}`, newItem);
+        await showMessage("infoBox", `Meme ${newItem.title} updated`);
         e.target.reset();
         ctx.page.redirect(`/details/${id}`);
     }
@@ -48,7 +54,8 @@ export async function editPage(ctx) {
     try {
         item = await get(`/data/memes/${id}`);
     } catch (err) {
-        alert(err.message);
+        if (err.message) showMessage("errorBox", err.message);
+        else showMessage("errorBox", err);
     }
 
     ctx.render(template(item, onEdit));
