2c4af7edc779c6299b1e8af854ea48b1f3e06cec
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-02-28 18:07:59 +0200

    Add form validation and proper messages during meme creation

--- 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/create.js
+++ 19. JS Applications Retake Exam - 1 Sept 2018 - Meme Longue/resources/src/views/app/create.js
@@ -1,5 +1,6 @@
 import {html} from "../../lib/lit-html.min.js";
 import {post} from "../../utils/api.js";
+import {showMessage} from "../../utils/utils.js";
 
 function template(onCreate) {
     return html`
@@ -35,14 +36,20 @@ export async function createPage(ctx) {
             imageUrl: formData.get('imageUrl').trim()
         }
 
-        if (Object.values(item).some((x) => !x)) return alert("All fields are required!");
+        if (Object.values(item).some((x) => !x)) return showMessage("errorBox", "All fields are required!");
+        if (33 < item.title.length) return showMessage("errorBox", "The title length must not exceed 33 characters!");
+        if (item.description.length < 30 || 450 < item.description.length) return showMessage("errorBox", "The description length must not exceed 450 characters and should be at least 30!");
+        if (!item.imageUrl.startsWith("http://") && !item.imageUrl.startsWith("https://")) return showMessage("errorBox", "Link url should always start with “http”.");
 
         try {
+            showMessage("loadingBox", "Loading...");
             await post("/data/memes", item);
+            await showMessage("infoBox", "meme created.");
             e.target.reset();
             ctx.page.redirect('/app');
         } catch (err) {
-            alert(err.message);
+            if (err.message) showMessage("errorBox", err.message);
+            else showMessage("errorBox", err);
         }
     }
 
