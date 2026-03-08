10691482d4aed69c102c2e36806bd0b1dc168ff3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-08 23:41:55 +0200

    Add "Register page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
@@ -2,11 +2,13 @@ import page from "./lib/page.js";
 
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home";
+import {registerPage} from "./views/auth/register";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
+page("/register", guardRoute("guest"), registerPage);
 
 page.start();
