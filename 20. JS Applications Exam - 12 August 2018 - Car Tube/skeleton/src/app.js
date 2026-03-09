8e4d9b732da568ec3ee4225ad6ed810f471eaef5
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-09 17:17:50 +0200

    Add hard-coded "My Listings Page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
@@ -4,6 +4,7 @@ import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
+import {profilePage} from "./views/app/profile.js";
 
 setNavigation();
 
@@ -12,5 +13,5 @@ page("/index.html", "/");
 page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
-
+page("/my-listings", guardRoute("user"), profilePage);
 page.start();
