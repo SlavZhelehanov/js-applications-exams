a0e01c748fc6742fb58a4dfd65e934dde5c66f0c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-10 15:55:40 +0200

    Add hard-coded "Listing Details Page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
@@ -5,6 +5,7 @@ import {homePage} from "./views/home/home.js";
 import {registerPage} from "./views/auth/register.js";
 import {loginPage} from "./views/auth/login.js";
 import {profilePage} from "./views/app/profile.js";
+import {detailsPage} from "./views/app/details.js";
 
 setNavigation();
 
@@ -14,4 +15,6 @@ page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/my-listings", guardRoute("user"), profilePage);
+page("/details/:id", guardRoute("user"), detailsPage);
+
 page.start();
