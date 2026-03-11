066d34e58a6fbf2bc09751f0ed355f097e57f45f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-11 15:23:40 +0200

    Add "Edit Listing Page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/app.js
@@ -7,6 +7,7 @@ import {loginPage} from "./views/auth/login.js";
 import {profilePage} from "./views/app/profile.js";
 import {detailsPage} from "./views/app/details.js";
 import {createPage} from "./views/app/create.js";
+import {editPage} from "./views/app/edit.js";
 
 setNavigation();
 
@@ -18,5 +19,6 @@ page("/login", guardRoute("guest"), loginPage);
 page("/my-listings", guardRoute("user"), profilePage);
 page("/details/:id", guardRoute("user"), detailsPage);
 page("/create", guardRoute("user"), createPage);
+page("/edit/:id", guardRoute("user"), editPage);
 
 page.start();
