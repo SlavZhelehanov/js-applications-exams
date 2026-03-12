9823556df6a374cd1e902d116463b7f32c3aff04
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-12 21:41:14 +0200

    Login user

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/auth/login.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/auth/login.js
@@ -1,9 +1,11 @@
 import {html} from '../../lib/lit-html.min.js';
+import {post} from "../../utils/api.js";
+import {saveUserData} from "../../utils/utils.js";
 
-function template() {
+function template(onLogin) {
     return html`
         <div id="login">
-            <form action="#" method="post">
+            <form @submit=${onLogin}>
                 <div class="container">
                     <h1>Login</h1>
                     <p>Please enter your credentials.</p>
@@ -19,12 +21,38 @@ function template() {
 
                 <div class="container signin">
                     <p>Dont have an account?
-                        <a href="#">Sign up</a>.</p>
+                        <a href="/register">Sign up</a>.</p>
                 </div>
             </form>
         </div>`;
 }
 
 export function loginPage(ctx) {
-    ctx.render(template());
+    async function onLogin(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        if (username.trim() === '' || password.trim() === '') return alert('All fields are required!');
+        if (/^[A-Za-z]{3,}$/.test(username)) return alert('A username should be at least 3 characters long and should contain only english alphabet letters.');
+        if (/^[A-Za-z]{6,}$/.test(password)) return alert('A user‘s password should be at least 6 characters long and should contain only english alphabet letters and digits. ');
+
+        try {
+            const user = await post("/users/login", {username, password});
+
+            if (399 < user.status) throw user.statusText;
+
+            saveUserData(user);
+            await alert("Login successful.");
+            e.target.reset();
+            ctx.setNavigation();
+            ctx.page.redirect('/');
+        } catch (err) {
+            if (err.message) alert(err.message);
+            else alert(err);
+        }
+    }
+
+    ctx.render(template(onLogin));
 }
