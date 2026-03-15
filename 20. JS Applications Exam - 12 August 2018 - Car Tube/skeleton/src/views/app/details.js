04038d2621578316f1fecb5f472977b04095a91d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-15 14:39:01 +0200

    Show correct data in "Details Page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/app/details.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/app/details.js
@@ -1,17 +1,18 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(item) {
     return html`
         <div class="listing-details">
             <div class="my-listing-details">
-                <p id="auto-title">Audi a3 много запазено</p>
-                <img src="https://i.imgur.com/drIOsYl.jpg">
+                <p id="auto-title">${item.title}</p>
+                <img src="${item.imageUrl}">
                 <div class="listing-props">
-                    <h2>Brand: Audi</h2>
-                    <h3>Model: a3</h3>
-                    <h3>Year: 1998</h3>
-                    <h3>Fuel: Gasoline</h3>
-                    <h3>Price: 2500$</h3>
+                    <h2>Brand: ${item.brand}</h2>
+                    <h3>Model: ${item.model}</h3>
+                    <h3>Year: ${item.year}</h3>
+                    <h3>Fuel: ${item.fuelType}</h3>
+                    <h3>Price: ${item.price}$</h3>
                 </div>
                 <div class="listings-buttons">
 
@@ -21,12 +22,21 @@ function template() {
 
                 </div>
                 <p id="description-title">Description:</p>
-                <p id="description-para">Не съм навъртал на колата 100000км в никакъв случай. Като нова е</p>
-
+                <p id="description-para">${item.description}</p>
             </div>
         </div>`;
 }
 
 export async function detailsPage(ctx) {
-    ctx.render(template());
+    const id = ctx.params.id;
+    let item = {};
+
+    try {
+        item = await get(`/data/cars/${id}`);
+    } catch (err) {
+        if (err.message) alert(err.message);
+        else alert(err);
+    }
+
+    ctx.render(template(item));
 }
