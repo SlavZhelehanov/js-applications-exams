923694fdd353be92856e34b243ace4760612f919
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-16 17:49:56 +0200

    Show correct data in "My Listings Page"

--- 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/app/profile.js
+++ 20. JS Applications Exam - 12 August 2018 - Car Tube/skeleton/src/views/app/profile.js
@@ -1,31 +1,45 @@
 import {html} from '../../lib/lit-html.min.js';
+import {get} from "../../utils/api.js";
 
-function template() {
+function template(items) {
     return html`
         <div class="my-listings">
             <h1>My car listings</h1>
             <div class="car-listings">
-                <div class="my-listing">
-                    <p id="listing-title">Audi a3 много запазено</p>
-                    <img src="https://i.imgur.com/drIOsYl.jpg">
+                ${0 < items.length
+                        ? items.map(i => html`
+                            <div class="my-listing">
+                                <p id="listing-title">${i.title}</p>
+                                <img src=${i.imageUrl}>
 
-                    <div class="listing-props">
-                        <h2>Brand: Audi</h2>
-                        <h3>Model: a3</h3>
-                        <h3>Year: 1998</h3>
-                        <h3>Price: 2500$</h3>
-                    </div>
-                    <div class="my-listing-buttons">
-                        <a href="#" class="my-button-list">Details</a>
-                        <a href="#" class="my-button-list">Edit</a>
-                        <a href="#" class="my-button-list">Delete</a>
-                    </div>
-                </div>
-                <p class="no-cars"> No cars in database.</p>
+                                <div class="listing-props">
+                                    <h2>Brand: ${i.brand}</h2>
+                                    <h3>Model: ${i.model}</h3>
+                                    <h3>Year: ${i.year}</h3>
+                                    <h3>Price: ${i.price}$</h3>
+                                </div>
+                                <div class="my-listing-buttons">
+                                    <a href="/details/${i._id}" class="my-button-list">Details</a>
+                                    <a href="/edit/${i._id}" class="my-button-list">Edit</a>
+                                    <a href="#" class="my-button-list">Delete</a>
+                                </div>
+                            </div>`)
+                        : html`<p class="no-cars"> No cars in database.</p>`
+                }
             </div>
         </div>`;
 }
 
 export async function profilePage(ctx) {
-    ctx.render(template());
+    const userId = ctx.userData._id;
+    let items = [];
+
+    try {
+        items = await get(`/data/cars?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
+    } catch (err) {
+        if (err.message) alert(err.message);
+        else alert(err);
+    }
+
+    ctx.render(template(items));
 }
