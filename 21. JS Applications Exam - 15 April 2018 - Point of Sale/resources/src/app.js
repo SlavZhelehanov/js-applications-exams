fc8119dbe6451f93886184c9da2f67268ceea6a8
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-03-25 15:04:39 +0200

    Add basic skeleton

---
+++ 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
@@ -0,0 +1,10 @@
+import page from "./lib/page.js";
+
+import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
+
+// setNavigation();
+
+page(decorateCTX);
+page("/index.html", "/");
+
+page.start();
