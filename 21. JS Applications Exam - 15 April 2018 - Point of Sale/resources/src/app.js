de9f2b2b4a737d96d3d379710387f2552c24ddcd
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-04-01 12:44:47 +0300

    Add hard coded "Details page"

--- 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
+++ 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
@@ -3,6 +3,7 @@ import page from "./lib/page.js";
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard";
+import {detailsPage} from "./views/app/details";
 
 setNavigation();
 
@@ -10,4 +11,5 @@ page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
 page("/dashboard", guardRoute("user"), dashboardPage);
+page("/details/:id", guardRoute("user"), detailsPage);
 page.start();
