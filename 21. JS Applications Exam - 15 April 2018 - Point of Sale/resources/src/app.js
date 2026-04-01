769ff2d709e9714eb7372c21af307f2b70a9d41f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-04-01 12:42:14 +0300

    Add hard coded "Catalog Page"

--- 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
+++ 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
@@ -1,12 +1,13 @@
 import page from "./lib/page.js";
 
-import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
-import { homePage } from "./views/home/home.js";
+import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
+import {homePage} from "./views/home/home.js";
+import {dashboardPage} from "./views/app/dashboard";
 
 setNavigation();
 
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
-
+page("/dashboard", guardRoute("user"), dashboardPage);
 page.start();
