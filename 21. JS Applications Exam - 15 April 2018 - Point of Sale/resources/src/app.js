88e8412545215753626ce6f5ae15983e4d8bb55a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-04-02 12:57:54 +0300

    Logout a User

--- 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
+++ 21. JS Applications Exam - 15 April 2018 - Point of Sale/resources/src/app.js
@@ -4,6 +4,7 @@ import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
 import {dashboardPage} from "./views/app/dashboard.js";
 import {detailsPage} from "./views/app/details.js";
+import {logoutAction} from "./views/auth/logout.js";
 
 setNavigation();
 
@@ -12,4 +13,5 @@ page("/index.html", "/");
 page("/", homePage);
 page("/dashboard", guardRoute("user"), dashboardPage);
 page("/details/:id", guardRoute("user"), detailsPage);
+page("/logout", guardRoute("user"), logoutAction);
 page.start();
