2f68e567a938e04da5b02c87de83f0aed5440396
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-04-20 16:10:53 +0300

    Login User

--- 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/src/views/home/home.js
+++ 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/src/views/home/home.js
@@ -1,5 +1,5 @@
 import { html } from "../../lib/lit-html.min.js";
-import { post } from "../../utils/api.js";
+import { get, post } from "../../utils/api.js";
 import { getisLogin, setIsLogin, saveUserData } from "../../utils/utils.js";
 
 function register({ redirectTo, onRegister }) {
@@ -20,15 +20,15 @@ function register({ redirectTo, onRegister }) {
     </section>`;
 }
 
-function login({ redirectTo }) {
+function login({ redirectTo, onLogin }) {
     return html`
         <section id="viewLogin">
         <div class="content">
-            <form id="formLogin" class="form">
+            <form id="formLogin" class="form" method='post' @submit=${onLogin}>
                 <label>Username</label>
-                <input name="username" type="text">
+                <input name="username-login" type="text">
                 <label>Password</label>
-                <input name="password" type="password">
+                <input name="password-login" type="password">
                 <input id="btnLogin" value="Sign In" type="submit">
                 <a @click=${() => redirectTo('register')} href="javascript:void(0)">Register</a>
             </form>
@@ -74,6 +74,33 @@ export async function homePage(ctx) {
 
         return ctx.render(register({ redirectTo, onRegister }));
     } else if (!isAuth && isLogin) {
-        return ctx.render(login({ redirectTo }));
+        async function onLogin(e) {
+            e.preventDefault();
+            const formData = new FormData(e.target);
+            const username = formData.get('username-login');
+            const password = formData.get('password-login');
+
+            if (username.trim() === '' || password.trim() === '') return alert('All fields are required!');
+            if (typeof username !== 'string' || username.length < 5) return alert('A username should be a string with at least 5 characters long.');
+
+            try {
+                const user = await post("/users/login", { username, password });
+
+                if (399 < user.status) throw user.statusText;
+
+                saveUserData(user);
+                alert('Login successful');
+                e.target.reset();
+                ctx.setNavigation();
+                ctx.page.redirect('/');
+            } catch (err) {
+                if (err.message) alert(err.message);
+                else alert(err);
+            }
+        }
+
+        return ctx.render(login({ redirectTo, onLogin }));
+    }
+
     }
 }
