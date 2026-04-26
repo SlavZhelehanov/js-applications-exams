54cc3b066618e44f1c59cfeb7d6d2a67aec6f30d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-04-26 17:07:53 +0300

    Show correct data in "Discover Page"

--- 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/src/views/auth/discover.js
+++ 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/src/views/auth/discover.js
@@ -1,22 +1,24 @@
 import { html } from "../../lib/lit-html.min.js";
-import { get, post } from "../../utils/api.js";
-import { getisLogin, setIsLogin, saveUserData } from "../../utils/utils.js";
+import { get } from "../../utils/api.js";
+import { getisLogin } from "../../utils/utils.js";
 
-function template() {
+function template(users) {
     return html`
         <section id="viewDiscover">
         <div class="content">
             <div class="chirps">
                 <h2 class="titlebar">Discover</h2>
                 <div id="userlist">
-                    <div class="userbox">
-                        <div><a href="#" class="chirp-author">vako</a></div>
+                    ${0 < users.length
+            ? users.map(u => html`<div class="userbox">
+                        <div><a href="/profile/${u._id}" class="chirp-author">${u.username}</a></div>
 
                         <div class="user-details">
-                            <span>3 followers</span>
+                            <span>${u.followers} followers</span>
                         </div>
-                    </div>
-                    <!-- TODO Load more user profiles -->
+                    </div>`)
+            : null
+        }
                 </div>
             </div>
         </div>
@@ -24,6 +26,17 @@ function template() {
 }
 
 export async function discoverPage(ctx) {
-    const isAuth = !!ctx.userData, isLogin = getisLogin();
-    return ctx.render(template());
+    const isAuth = ctx.userData, isLogin = getisLogin();
+    let users = [];
+
+    try {
+        const data = await get('/jsonstore/users');
+        Object.keys(data).forEach(id => {
+            if (data[id].username !== isAuth.username) users.push({ username: data[id].username, _id: data[id]._id, followers: data[id].followers.length });
+        });
+    } catch (error) {
+        if (err.message) alert(err.message);
+        else alert(err);
+    }
+    return ctx.render(template(users));
 }
