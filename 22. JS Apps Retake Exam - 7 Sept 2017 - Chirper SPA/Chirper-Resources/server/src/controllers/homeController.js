b8a028b890407e49815b993bf15bad608dafc4ca
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-05-06 16:36:31 +0300

    Populate chirps data

--- 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/server/src/controllers/homeController.js
+++ 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/server/src/controllers/homeController.js
@@ -1,24 +1,23 @@
 import {Router} from "express";
 
 import {parseErrorMessage} from "../util/parseErrorMessage.js";
-import User from "../models/User.js";
 import usersSeed from "./users.js";
+import Chirp from "../models/Chirp.js";
+import chirpsSeed from "./chirps.js";
 
 const homeController = Router();
-const PASSWORD = "123456";
 
 // HOME
 homeController.get("/", async (req, res) => {
     try {
-        for (let i = 0; i < usersSeed.length; i++) {
-            const user = await User.create({
-                username: usersSeed[i].username,
-                // email: usersSeed[i].email,
-                followers: usersSeed[i].subscriptions,
-                userId: usersSeed[i]._id,
-                password: PASSWORD,
+        for (let i = 0; i < chirpsSeed.length; i++) {
+            const {username} = usersSeed.find(u => u._id === chirpsSeed[i]._acl.creator);
+            const chirp = await Chirp.create({
+                text: chirpsSeed[i].text,
+                author: username,
+                creator: chirpsSeed[i]._acl.creator,
             });
-            console.log(user);
+            console.log(chirp);
         }
         return res.status(200).json({message: "Welcome to the API"});
     } catch (error) {
