b36c2cbe8fe1e9fac80fed86bdc5fe164078d85c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-05-07 17:46:01 +0300

    Add Register View

---
+++ 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/client/src/views/home/home.js
@@ -0,0 +1,23 @@
+import { html } from "../../lib/lit-html.min.js";
+
+function register() {
+    return html`
+        <section id="viewRegister">
+        <div class="content">
+            <form class="form" id="formRegister">
+                <label>Username</label>
+                <input name="username" type="text">
+                <label>Password</label>
+                <input name="password" type="password">
+                <label>Repeat Password</label>
+                <input name="repeatPass" type="password">
+                <input id="btnRegister" value="Register" type="submit">
+                <a href="#">Log in</a>
+            </form>
+        </div>
+    </section>`;
+}
+
+export async function homePage(ctx) {
+    ctx.render(register());
+}
