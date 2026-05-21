ce4709436647f0cd5ec4d3b97fa8e281b2c047cd
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-05-21 17:17:43 +0300

    Show Logout messages

--- 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/client/src/views/auth/logout.js
+++ 22. JS Apps Retake Exam - 7 Sept 2017 - Chirper SPA/Chirper-Resources/client/src/views/auth/logout.js
@@ -1,14 +1,15 @@
 import { get } from "../../utils/api.js";
-import { clearUserData } from "../../utils/utils.js";
+import { clearUserData, showNotification } from "../../utils/utils.js";
 
 export async function logoutAction(ctx) {
     try {
         // await get("/auth/logout");
         clearUserData();
+        showNotification('info', 'Logout successful.');
         ctx.setNavigation();
     } catch (err) {
-        if (err.message) alert(err.message);
-        else alert(err);
+        if (err.message) showNotification('error', err.message);
+        else showNotification('error', err);
     }
     return ctx.page.redirect('/');
 }
