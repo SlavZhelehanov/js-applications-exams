85a3ab15c87ed82c1e6d17e2b3d27a2ba18bad9a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-05-27 13:02:01 +0300

    Add basic frontend skeleton

---
+++ 23. JS Applications Exam - 20 August 2017 - SeentIt/SeentIt/client/src/utils/api.js
@@ -0,0 +1,36 @@
+import { getUserData } from "./utils.js";
+
+// const URL = 'http://localhost:3030';
+const URL = 'http://localhost:5050';
+
+async function request(method, url, data) {
+    // const options = { method, credentials: "include", headers: {} };
+    const options = { method, headers: {} };
+    const userData = getUserData();    
+
+    if (userData) {
+        options.headers['x-authorization'] = userData.token;
+        // options.headers['x-authorization'] = userData['id'];
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(URL + url, options);
+
+    if (response.ok === false) {
+        throw await response.json();
+    }
+    try {
+        return await response.json();
+    } catch (err) {
+        return response;
+    }
+}
+
+export const get = request.bind(null, "get");
+export const post = request.bind(null, "post");
+export const put = request.bind(null, 'put');
+export const del = request.bind(null, "delete");
