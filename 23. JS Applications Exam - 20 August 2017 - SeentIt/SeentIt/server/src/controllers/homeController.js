c2b22113db382975a858430975823de2abfc6eda
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-05-28 12:29:34 +0300

    Add hard coded "Catalog View"

--- 23. JS Applications Exam - 20 August 2017 - SeentIt/SeentIt/server/src/controllers/homeController.js
+++ 23. JS Applications Exam - 20 August 2017 - SeentIt/SeentIt/server/src/controllers/homeController.js
@@ -1,24 +1,13 @@
 import {Router} from "express";
 
 import {parseErrorMessage} from "../util/parseErrorMessage.js";
-import usersSeed from "./users.js";
-import Chirp from "../models/Chirp.js";
-import chirpsSeed from "./chirps.js";
 
 const homeController = Router();
 
 // HOME
 homeController.get("/", async (req, res) => {
     try {
-        for (let i = 0; i < chirpsSeed.length; i++) {
-            const {username} = usersSeed.find(u => u._id === chirpsSeed[i]._acl.creator);
-            const chirp = await Chirp.create({
-                text: chirpsSeed[i].text,
-                author: username,
-                creator: chirpsSeed[i]._acl.creator,
-            });
-            console.log(chirp);
-        }
+        
         return res.status(200).json({message: "Welcome to the API"});
     } catch (error) {
         return res.status(500).json({messages: parseErrorMessage(error)});
