28ca762cf6e38185f4cb9c4c15c5d1aa9009ead8
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-06-03 16:01:22 +0300

    Populate user db with given resources

--- 23. JS Applications Exam - 20 August 2017 - SeentIt/SeentIt/server/src/controllers/homeController.js
+++ 23. JS Applications Exam - 20 August 2017 - SeentIt/SeentIt/server/src/controllers/homeController.js
@@ -1,12 +1,23 @@
 import {Router} from "express";
 
 import {parseErrorMessage} from "../util/parseErrorMessage.js";
+import users from "../../import-json/users.js";
+import User from "../models/User.js";
 
 const homeController = Router();
 
 // HOME
 homeController.get("/", async (req, res) => {
     try {
+        for (const user of users) {
+            const newUser = await User.create({
+                username: user.username,
+                userId: user._id,
+                password: "123456",
+                createdAt: user._kmd.ect,
+            })
+            console.log(newUser);
+        }
 
         return res.status(200).json({message: "Welcome to the API"});
     } catch (error) {
