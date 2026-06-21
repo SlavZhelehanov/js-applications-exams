3513bca387ff56c19596f57798622518571c0772
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-06-21 15:44:53 +0300

    Set Navigation

--- 24. JS Applications Retake Exam - 22 December 2016 - Market/resources/client/src/app.js
+++ 24. JS Applications Retake Exam - 22 December 2016 - Market/resources/client/src/app.js
@@ -1,3 +1,6 @@
 import page from "./lib/page.mjs";
+import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
+setNavigation();
 page(decorateCTX);
 page("/index.html", "/");
+page.start();
