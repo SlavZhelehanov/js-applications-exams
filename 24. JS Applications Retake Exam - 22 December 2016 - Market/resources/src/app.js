6edea0a3a8eb5c4bcf486704804d949d519c8374
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-06-23 13:18:04 +0300

    Add 'Login Page'

--- 24. JS Applications Retake Exam - 22 December 2016 - Market/resources/src/app.js
+++ 24. JS Applications Retake Exam - 22 December 2016 - Market/resources/src/app.js
@@ -1,9 +1,11 @@
 import page from "./lib/page.mjs";
 
 import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
+import { loginPage } from "./views/auth/login.js";
 import { homePage } from "./views/home/home.js";
 setNavigation();
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
+page("/login", guardRoute('guest'), loginPage);
 page.start();
