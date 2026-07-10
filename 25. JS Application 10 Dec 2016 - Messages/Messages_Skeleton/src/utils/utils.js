06ee58ce08228c8fdab949ae167590b243fe5521
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-10 15:11:00 +0300

    Set navigation

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/utils/utils.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/utils/utils.js
@@ -1,6 +1,32 @@
-import {render} from "../lib/lit-html.min.js";
+import { render } from "../lib/lit-html.min.js";
 import page from "../lib/page.mjs";
 
+const item = "userData";
+
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem(item));
+}
+
+export function setNavigation() {
+    const isLoggedIn = Boolean(getUserData());
+    const header = document.getElementById('menu');
+    const username = getUserData() ? getUserData().username : null;
+
+    !isLoggedIn
+        ? header.innerHTML = `
+        <a href="/" class="anonymous" id="linkMenuAppHome">Home</a>
+        <a href="/login" class="anonymous" id="linkMenuLogin">Login</a>
+        <a href="/register" class="anonymous" id="linkMenuRegister">Register</a>
+        ` : header.innerHTML = `
+        <a href="/" class="useronly" id="linkMenuUserHome">Home</a>
+        <a href="/my-messages" class="useronly" id="linkMenuMyMessages">My Messages</a>
+        <a href="/archive" class="useronly" id="linkMenuArchiveSent">Archive (Sent)</a>
+        <a href="/send" class="useronly" id="linkMenuSendMessage">Send Message</a>
+        <a href="/logout" class="useronly" id="linkMenuLogout">Logout</a>
+
+        <span class="useronly" id="spanMenuLoggedInUser">Welcome, ${username}!</span>
+        `;
+}
 
 export function decorateCTX(ctx, next) {
     const main = document.getElementsByTagName('main')[0];
