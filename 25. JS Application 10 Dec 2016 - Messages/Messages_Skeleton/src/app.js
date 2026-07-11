e4867c1bd929e64a77cebdbe972f55e0b3814cdf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-11 17:41:28 +0300

    Add "Login View"

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/app.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/app.js
@@ -2,8 +2,12 @@ import page from "./lib/page.mjs";
 
 import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
 import {homePage} from "./views/home/home.js";
+import {loginPage} from "./views/auth/login.js";
 
 setNavigation();
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
+page("/login", guardRoute("guest"), loginPage);
+
+page.start();
