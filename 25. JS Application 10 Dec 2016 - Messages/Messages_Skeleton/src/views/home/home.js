2444883d8720b9b9357c640676d1fdaaaf693f2f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-13 09:41:20 +0300

    Add "User Home View"

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/home/home.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/home/home.js
@@ -1,4 +1,5 @@
 import { html } from "../../lib/lit-html.min.js";
+import { getUserData } from "../../utils/utils.js";
 
 function template() {
     return html`
@@ -8,6 +9,22 @@ function template() {
         </section>`;
 }
 
+function dashboard() {
+    return html`
+        <section id="viewUserHome">
+            <h1 id="viewUserHomeHeading">Welcome, {user}!</h1>
+            <a href="/me-messages" id="linkUserHomeMyMessages">My Messages</a>
+            <a href="/send" id="linkUserHomeSendMessage">Send Message</a>
+            <a href="/archive" id="linkUserHomeArchiveSent">Archive (Sent)</a>
+        </section>`;
+}
+
 export async function homePage(ctx) {
-    ctx.render(template());
+    const isAuth = getUserData();
+
+    if (isAuth) {
+        ctx.render(dashboard());
+    } else {
+        ctx.render(template());
+    }
 }
