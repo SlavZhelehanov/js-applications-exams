2513f890079e79168dd69afc5351effea5f7a0e6
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-16 16:06:13 +0300

    Add hard-coded "My Messages View"

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/app.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/app.js
@@ -1,15 +1,21 @@
 import page from "./lib/page.mjs";
 
-import {decorateCTX, guardRoute, setNavigation} from "./utils/utils.js";
-import {homePage} from "./views/home/home.js";
-import {loginPage} from "./views/auth/login.js";
-import {registerPage} from "./views/auth/register.js";
+import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
+import { homePage } from "./views/home/home.js";
+import { loginPage } from "./views/auth/login.js";
+import { registerPage } from "./views/auth/register.js";
+import { logoutAction } from "./views/auth/logout.js";
+import { myMessagesPage } from "./views/app/my-messages.js";
 
 setNavigation();
+
 page(decorateCTX);
 page("/index.html", "/");
 page("/", homePage);
+page("/app", dashboardPage);
 page("/login", guardRoute("guest"), loginPage);
 page("/register", guardRoute("guest"), registerPage);
+page("/logout", guardRoute("user"), logoutAction);
+page("/my-messages", guardRoute("user"), myMessagesPage);
 
 page.start();
