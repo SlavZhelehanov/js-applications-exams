4425847e2cc762f090bfcfdd3fd5ceaba754855a
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-18 17:57:09 +0300

    Fix "Sen/Create View"

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/app/create.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/app/create.js
@@ -1,3 +1,26 @@
+import { html } from "../../lib/lit-html.min.js";
+import { get, post } from "../../utils/api.js";
+
+function template({ recipients, onCreate }) {
+    return html`<section id="viewSendMessage">
+            <h1>Send Message</h1>
+            <form id="formSendMessage" @submit=${onCreate}>
+                <div>Recipient:</div>
+                <div>
+                    <select name="recipient" required id="msgRecipientUsername">
+                            <option id="" value="">Изберете потребител</option>
+                        ${recipients.map(user => html`
+                            <option id=${user.userId} value=${user.username}>${user.username}</option>
+                        `)}
+                    </select>
+                </div>
+                <div>Message Text:</div>
+                <div><input type="text" name="text" required id="msgText" /></div>
+                <div><input type="submit" value="Send" /></div>
+            </form>
+        </section>`;
+}
+
 export async function createPage(ctx) {
     let recipients = [];
 
@@ -15,10 +38,7 @@ export async function createPage(ctx) {
             message: formData.get('text')?.trim()
         };
 
-
-        if (Object.values(item).some((x) => !x)) {
-            return alert("All fields are required!");
-        }
+        if (Object.values(item).some((x) => !x)) return alert("All fields are required!");
 
         try {
             await post("/app", item);
@@ -35,5 +55,5 @@ export async function createPage(ctx) {
         alert(err.message);
     }
 
-    ctx.render(template({ recipients, onCreate }));
+    return ctx.render(template({ recipients, onCreate }));
 }
