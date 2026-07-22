80c54b613c93fc5fce45eb05aa758179f4df7ce2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-07-22 12:33:08 +0300

    Show correct messages during user's logout

--- 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/auth/logout.js
+++ 25. JS Application 10 Dec 2016 - Messages/Messages_Skeleton/src/views/auth/logout.js
@@ -1,14 +1,17 @@
 import { get } from "../../utils/api.js";
-import { clearUserData } from "../../utils/utils.js";
+import { clearUserData, showError, showInfo, showLoading } from "../../utils/utils.js";
 
 export async function logoutAction(ctx) {
     try {
+        showLoading();
         await get("/auth/logout");
         clearUserData();
+        showInfo("Logout successful.");
         ctx.setNavigation();
         ctx.page.redirect('/');
     } catch (err) {
-        alert(err.message);
+        if (err.message) showError(err.message);
+        else showError(err);
     }
     return ctx.page.redirect('/');
 }
