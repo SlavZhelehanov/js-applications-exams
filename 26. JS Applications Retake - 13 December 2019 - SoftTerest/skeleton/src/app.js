9e095b3d3b4aed16c80cd4e0aa35a15402dc15cf
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-08-03 12:53:21 +0300

    Add "Create page"

--- 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/app.js
+++ 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/app.js
@@ -4,6 +4,7 @@ import { decorateCTX, guardRoute, setNavigation } from "./utils/utils.js";
 import { homePage } from "./views/home/home.js";
 import { loginPage } from "./views/auth/login.js";
 import { registerPage } from "./views/auth/register.js";
+import { createPage } from "./views/app/create.js";
 import { logoutAction } from "./views/auth/logout.js";
 
 setNavigation();
@@ -14,4 +15,5 @@ page("/", homePage);
 page("/login", guardRoute("guest"), loginPage);
 page("/register", guardRoute("guest"), registerPage);
 page("/logout", guardRoute("user"), logoutAction);
+page("/create", guardRoute("user"), createPage);
 page.start();
