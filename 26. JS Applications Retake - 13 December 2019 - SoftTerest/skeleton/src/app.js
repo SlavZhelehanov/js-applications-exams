ff41c400a89354b585c0cc053b4164c1cb7acaf5
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-08-05 12:44:48 +0300

    Add hard-coded "Details Page"

--- 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/app.js
+++ 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/app.js
@@ -6,6 +6,7 @@ import { loginPage } from "./views/auth/login.js";
 import { registerPage } from "./views/auth/register.js";
 import { createPage } from "./views/app/create.js";
 import { logoutAction } from "./views/auth/logout.js";
+import { detailsPage } from "./views/app/details.js";
 
 setNavigation();
 
@@ -16,4 +17,6 @@ page("/login", guardRoute("guest"), loginPage);
 page("/register", guardRoute("guest"), registerPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/create", guardRoute("user"), createPage);
+page("/:id/details", guardRoute("user"), detailsPage);
+
 page.start();
