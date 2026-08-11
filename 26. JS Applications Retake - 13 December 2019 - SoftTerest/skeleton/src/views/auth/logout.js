62f724cf0196ff14e0e229a3ec082d87e4d29439
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-08-11 12:10:22 +0300

    Show correct messages during user's logout

--- 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/views/auth/logout.js
+++ 26. JS Applications Retake - 13 December 2019 - SoftTerest/skeleton/src/views/auth/logout.js
@@ -1,15 +1,17 @@
 import { get } from "../../utils/api.js";
-import { clearUserData } from "../../utils/utils.js";
+import { clearUserData, showError, showInfo, showLoading } from "../../utils/utils.js";
 
 export async function logoutAction(ctx) {
     try {
+        showLoading();
         await get("/auth/logout");
         clearUserData();
+        showInfo("Logout successful.");
         ctx.setNavigation();
         ctx.page.redirect('/');
     } catch (err) {
-        if (err.message) alert(err.message);
-        alert(err);
+        if (err.message) showError(err.message);
+        else showError(err);
     }
     return ctx.page.redirect('/');
 }
