8919cc0667e6b1f77c916e45b2ebe87237a9b420
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-08-26 17:04:09 +0300

    Add hard coded "Details Page"

--- 27. JS Applications Exam - 05 Apr 2020 - SoftWiki/SoftWiki/src/app.js
+++ 27. JS Applications Exam - 05 Apr 2020 - SoftWiki/SoftWiki/src/app.js
@@ -5,6 +5,7 @@ import { homePage } from "./views/home/home.js";
 import { registerPage } from "./views/auth/register.js";
 import { createPage } from "./views/app/create.js";
 import { logoutAction } from "./views/auth/logout.js";
+import { detailsPage } from "./views/app/details.js";
 
 setNavigation();
 page(decorateCTX);
@@ -13,3 +14,6 @@ page("/", homePage);
 page("/register", guardRoute("guest"), registerPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/create", guardRoute("user"), createPage);
+page("/:id/details", guardRoute("user"), detailsPage);
+
+page.start();
