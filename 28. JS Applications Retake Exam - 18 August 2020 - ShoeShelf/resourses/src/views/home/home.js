8fbcc837ad1860a7f0890437a8de48f01690cbf7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-09-09 16:46:25 +0300

    Add "Guest Home Page"

---
+++ 28. JS Applications Retake Exam - 18 August 2020 - ShoeShelf/resourses/src/views/home/home.js
@@ -0,0 +1,20 @@
+import { html } from "../../lib/lit-html.min.js";
+
+function template() {
+    return html`
+        <div class="container">
+            <div class="about-us">
+                <div>
+                    <img src="../public/shoes.jpg" alt="">
+                    <img src="../public/shoes2.jpg" alt="">
+                </div>
+                <p>
+                    <a href="">Register Now</a> and Try it!
+                </p>
+            </div>
+        </div>`;
+}
+
+export function homePage(ctx) {
+    ctx.render(template());
+}
