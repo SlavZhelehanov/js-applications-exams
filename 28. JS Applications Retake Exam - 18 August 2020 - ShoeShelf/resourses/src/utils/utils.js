752a8d202ed6dd8375c474c0dc0b331bd0175d47
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-09-11 15:52:49 +0300

    Fix Navigation

--- 28. JS Applications Retake Exam - 18 August 2020 - ShoeShelf/resourses/src/utils/utils.js
+++ 28. JS Applications Retake Exam - 18 August 2020 - ShoeShelf/resourses/src/utils/utils.js
@@ -13,8 +13,8 @@ export function setNavigation() {
     const ul = document.getElementsByTagName('ul')[0];
 
     isLoggedIn
-        ? ul.innerHTML = `<li><a href="">Create new offer</a></li><li><a href=""><img src="./public/sneakers.png" alt=""></a></li><li>Welcome, ${user?.email || 'email'} | <a  href="">Logout</a></li>`
-        : ul.innerHTML = `<li class="site-logo">Shoe</li><li><a href=""><img src="./public/sneakers.png" alt=""></a></li><li class="site-logo">Shelf</li>`
+        ? ul.innerHTML = `<li><a href="/create">Create new offer</a></li><li><a href="/"><img src="./public/sneakers.png" alt=""></a></li><li>Welcome, ${user?.email || 'email'} | <a  href="/logout">Logout</a></li>`
+        : ul.innerHTML = `<li class="site-logo">Shoe</li><li><a href="/"><img src="./public/sneakers.png" alt=""></a></li><li class="site-logo">Shelf</li>`
 }
 
 export function decorateCTX(ctx, next) {
