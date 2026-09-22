0683555a85fe12b8e40d20dcb09f30ec491d5784
SlavZhelehanov <slav.zhelehanov@gmail.com>
2026-09-22 15:40:50 +0300

    Add hard-coded "Details Page"

--- 28. JS Applications Retake Exam - 18 August 2020 - ShoeShelf/resourses/src/app.js
+++ 28. JS Applications Retake Exam - 18 August 2020 - ShoeShelf/resourses/src/app.js
@@ -6,6 +6,7 @@ import { registerPage } from "./views/auth/register.js";
 import { logoutAction } from "./views/auth/logout.js";
 import { loginPage } from "./views/auth/login.js";
 import { createPage } from "./views/app/create.js";
+import { detailsPage } from "./views/app/details.js";
 
 setNavigation();
 
@@ -16,5 +17,6 @@ page("/register", guardRoute("guest"), registerPage);
 page("/logout", guardRoute("user"), logoutAction);
 page("/login", guardRoute("guest"), loginPage);
 page("/create", guardRoute("user"), createPage);
+page("/:id/details", detailsPage);
 
 page.start();
